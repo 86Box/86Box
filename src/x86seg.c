8d39f0ac7607a1809578d4490c09bf701443cbff
OBattler <oubattler@gmail.com>
2016-08-10 02:16:12 +0200

    General protection faults are only disabled if error is 0x82.

--- src/x86seg.c
+++ src/x86seg.c
@@ -145,7 +145,7 @@ void x86_doabrt(int x86_abrt)
 }
 void x86gpf(char *s, uint16_t error)
 {
-	return;
+	if (error == 0x82)  return;
         pclog("GPF %04X : %s\n", error, s);
         abrt = ABRT_GPF;
         abrt_error = error;
