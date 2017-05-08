769a2939d57446b859b4961be1cd7495940c639e
waltje <decwiz@yahoo.com>
2017-05-08 18:26:27 -0400

    Re-committed scsi_aha154x.h file - sorry !

---
+++ src/scsi_aha154x.h
@@ -0,0 +1,17 @@
+#ifndef SCSI_AHA154X_H
+# define SCSI_AHA154X_H
+
+
+typedef struct {
+    uint8_t	flags;		/* local flags */
+    uint8_t	bid;		/* board ID */
+    char	fwl, fwh;	/* firmware info */
+} aha_info;
+#define AHA_GLAG_MEMEN	0x01	/* BIOS Shadow RAM enabled */
+
+
+extern device_t aha1540b_device;
+extern device_t aha1542cf_device;
+  
+  
+#endif	/*SCSI_AHA154X_H*/
