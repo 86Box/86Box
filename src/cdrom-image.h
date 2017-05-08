df4fdb803bcb38977d0a6f4a5a502bac31e11e3d
OBattler <oubattler@gmail.com>
2017-05-08 16:46:28 +0200

    Recommitted the 4 files that waltje's file caused to disappear.

---
+++ src/cdrom-image.h
@@ -0,0 +1,25 @@
+/* Copyright holders: RichardG867, Tenshi
+   see COPYING for more details
+*/
+#ifndef CDROM_IMAGE_H
+#define CDROM_IMAGE_H
+
+/* this header file lists the functions provided by
+   various platform specific cdrom-ioctl files */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern int image_open(uint8_t id, wchar_t *fn);
+extern void image_reset(uint8_t id);
+
+extern void image_close(uint8_t id);
+
+extern void cdrom_set_null_handler(uint8_t id);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ! CDROM_IMAGE_H */
