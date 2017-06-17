8b03a29638e82cb11f004eab7ec85f0241d3358e
waltje <decwiz@yahoo.com>
2017-06-17 03:31:11 -0400

    Fix up the serial mess.  New mouse_serial.ch, new serial.h, updated (but old) serial.c.

--- src/mouse_serial.c
+++ src/mouse_serial.c
@@ -10,7 +10,7 @@
  *
  *		Based on the 86Box Serial Mouse driver as a framework.
  *
- * Version:	@(#)mouse_serial.c	1.0.3	2017/05/07
+ * Version:	@(#)mouse_serial.c	1.0.5	2017/06/18
  *
  * Author:	Fred N. van Kempen, <decwiz@yahoo.com>
  */
@@ -22,13 +22,18 @@
 #include "mouse_serial.h"
 
 
+#define SERMOUSE_TYPE_MSYSTEMS	1		/* Mouse Systems */
+#define SERMOUSE_TYPE_MICROSOFT	2		/* Microsoft */
+#define SERMOUSE_TYPE_LOGITECH	3		/* Logitech */
+
+
 typedef struct mouse_serial_t {
-    int		port;
+    int8_t	port,
+		type;
     int		pos,
 		delay;
     int		oldb;
     SERIAL	*serial;
-	int 	is_ms_format;
 } mouse_serial_t;
 
 
@@ -51,12 +56,16 @@ sermouse_timer(void *priv)
     mouse_serial_t *ms = (mouse_serial_t *)priv;
 
     ms->delay = 0;
-    if (ms->pos == -1) 
-	{
-		ms->pos = 0;
 
+    switch(ms->type) {
+	case SERMOUSE_TYPE_MICROSOFT:
 		/* This identifies a two-button Microsoft Serial mouse. */
-		serial_write_fifo(ms->serial, 'M');
+		serial_write_fifo(ms->serial, 'M', 1);
+		break;
+
+	default:
+		/* No action needed. */
+		break;
     }
 }
 
@@ -65,50 +74,60 @@ static uint8_t
 sermouse_poll(int x, int y, int z, int b, void *priv)
 {
     mouse_serial_t *ms = (mouse_serial_t *)priv;
-    uint8_t data[3];
+    uint8_t buff[16];
+    int len;
 
     if (!x && !y && b == ms->oldb) return(1);
 
     ms->oldb = b;
+
+    if (ms->type == SERMOUSE_TYPE_MSYSTEMS) y = -y;
+
     if (x>127) x = 127;
     if (y>127) y = 127;
     if (x<-128) x = -128;
     if (y<-128) y = -128;
 
-    /* Use Microsoft format. */
-    data[0] = 0x40;
-    data[0] |= (((y>>6)&3)<<2);
-    data[0] |= ((x>>6)&3);
-    if (b&1) data[0] |= 0x20;
-    if (b&2) data[0] |= 0x10;
-    data[1] = x & 0x3F;
-    data[2] = y & 0x3F;
+    len = 0;
+    switch(ms->type) {
+	case SERMOUSE_TYPE_MSYSTEMS:
+		buff[0] = 0x80;
+		buff[0] |= (b&0x01) ? 0x00 : 0x04;	/* left button */
+		buff[0] |= (b&0x02) ? 0x00 : 0x01;	/* middle button */
+		buff[0] |= (b&0x04) ? 0x00 : 0x02;	/* right button */
+		buff[1] = x;
+		buff[2] = y;
+		buff[3] = x;				/* same as byte 1 */
+		buff[4] = y;				/* same as byte 2 */
+		len = 5;
+		break;
+
+	case SERMOUSE_TYPE_MICROSOFT:
+		buff[0] = 0x40;
+		buff[0] |= (((y>>6)&03)<<2);
+		buff[0] |= ((x>>6)&03);
+		if (b&0x01) buff[0] |= 0x20;
+		if (b&0x02) buff[0] |= 0x10;
+		buff[1] = x & 0x3F;
+		buff[2] = y & 0x3F;
+		len = 3;
+		break;
+
+	case SERMOUSE_TYPE_LOGITECH:
+		break;
+    }
+
+#if 0
+    pclog("Mouse_Serial(%d): [", ms->type);
+    for (b=0; b<len; b++) pclog(" %02X", buff[b]);
+    pclog(" ] (%d)\n", len);
+#endif
 
     /* Send the packet to the bottom-half of the attached port. */
-#if 0
-    pclog("Mouse_Serial: data %02X %02X %02X\n", data[0], data[1], data[2]);
-#endif
-    serial_write_fifo(ms->serial, data[0]);
-    serial_write_fifo(ms->serial, data[1]);
-    serial_write_fifo(ms->serial, data[2]);
+    for (b=0; b<len; b++)
+	serial_write_fifo(ms->serial, buff[b], 1);
 
     return(0);
-}
-
-
-static void *
-sermouse_init(void)
-{
-    mouse_serial_t *ms = (mouse_serial_t *)malloc(sizeof(mouse_serial_t));
-    memset(ms, 0x00, sizeof(mouse_serial_t));
-    ms->port = SERMOUSE_PORT;
-
-    /* Attach a serial port to the mouse. */
-    ms->serial = serial_attach(ms->port, sermouse_callback, ms);
-
-    timer_add(sermouse_timer, &ms->delay, &ms->delay, ms);
-
-    return(ms);
 }
 
 
@@ -124,59 +143,14 @@ sermouse_close(void *priv)
 }
 
 
-mouse_t mouse_serial_microsoft = {
-    "Microsoft 2-button mouse (serial)",
-    "msserial",
-    MOUSE_TYPE_SERIAL,
-    sermouse_init,
-    sermouse_close,
-    sermouse_poll
-};
-
-static uint8_t
-mssystems_mouse_poll(int x, int y, int z, int b, void *priv)
-{
-    mouse_serial_t *ms = (mouse_serial_t *)priv;
-    uint8_t data[5];
-	
-    if (!x && !y && b == ms->oldb) return(1);
-
-    ms->oldb = b;
-	
-	y=-y;
-	
-    if (x>127) x = 127;
-    if (y>127) y = 127;
-    if (x<-128) x = -128;
-    if (y<-128) y = -128;
-
-	data[0] = 0x80;
-	data[0] |= (b & 0x01 ? 0x00 : 0x04); /*Left button*/
-	data[0] |= (b & 0x02 ? 0x00 : 0x01); /*Middle button*/
-	data[0] |= (b & 0x04 ? 0x00 : 0x02); /*Right button*/
-	data[1] = x;
-	data[2] = y;
-	data[3] = x;/*Same as byte 1*/
-	data[4] = y;/*Same as byte 2*/
-	
-	pclog("Mouse_Systems_Serial: data %02X %02X %02X\n", data[0], data[1], data[2]);
-	
-    serial_write_fifo(ms->serial, data[0]);
-    serial_write_fifo(ms->serial, data[1]);
-    serial_write_fifo(ms->serial, data[2]);
-	serial_write_fifo(ms->serial, data[3]);  
-	serial_write_fifo(ms->serial, data[4]);
-	
-	return(0);
-}
-
 static void *
-mssystems_mouse_init(void)
+sermouse_init(int type)
 {
     mouse_serial_t *ms = (mouse_serial_t *)malloc(sizeof(mouse_serial_t));
     memset(ms, 0x00, sizeof(mouse_serial_t));
     ms->port = SERMOUSE_PORT;
-	
+    ms->type = type;
+
     /* Attach a serial port to the mouse. */
     ms->serial = serial_attach(ms->port, sermouse_callback, ms);
 
@@ -185,11 +159,36 @@ mssystems_mouse_init(void)
     return(ms);
 }
 
+
+static void *
+sermouse_init_microsoft(void)
+{
+    return(sermouse_init(SERMOUSE_TYPE_MICROSOFT));
+}
+
+
+static void *
+sermouse_init_msystems(void)
+{
+    return(sermouse_init(SERMOUSE_TYPE_MSYSTEMS));
+}
+
+
 mouse_t mouse_msystems = {
     "Mouse Systems Mouse (serial)",
     "mssystems",
     MOUSE_TYPE_MSYSTEMS,
-    mssystems_mouse_init,
+    sermouse_init_msystems,
     sermouse_close,
-    mssystems_mouse_poll
+    sermouse_poll
+};
+
+
+mouse_t mouse_serial_microsoft = {
+    "Microsoft 2-button mouse (serial)",
+    "msserial",
+    MOUSE_TYPE_SERIAL,
+    sermouse_init_microsoft,
+    sermouse_close,
+    sermouse_poll
 };
