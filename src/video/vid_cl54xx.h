b5f0b46df5401c49df3dccfd12bdd68ad3bdcbc9
TC1995 <mariogplayer90@gmail.com>
2018-02-23 14:20:59 +0100

    Forgot to commit the updated cirrus header...

--- src/video/vid_cl54xx.h
+++ src/video/vid_cl54xx.h
@@ -3,8 +3,10 @@
 */
 extern device_t gd5428_isa_device;
 extern device_t gd5428_vlb_device;
-extern device_t gd5429_device;
+extern device_t gd5429_isa_device;
+extern device_t gd5429_vlb_device;
 extern device_t gd5430_vlb_device;
 extern device_t gd5430_pci_device;
+extern device_t gd5434_isa_device;
 extern device_t gd5434_vlb_device;
 extern device_t gd5434_pci_device;
