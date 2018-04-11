e7e9a0bfababc80d941f4fdaf499e71137300eec
basic2004 <basic2004@gmail.com>
2018-04-11 19:54:22 +0900

    Updated Nuked OPL3 1.7.4 to 1.8
    from Nukeykt:
    
    Nuked OPL3 v1.8 is out.
    Changelog:
    * New envelope generator.
    * Rhythm mode emulation is 100% correct now.
    
    These and other aspects of chip have been verified on YMF262 die shot, so emulation should be perfect now.

--- src/sound/nukedopl.h
+++ src/sound/nukedopl.h
@@ -1,7 +1,16 @@
-/* Copyright holders: Alexey Khokholov (Nuke.YKT)
-   see COPYING for more details
-*/
-
+//
+// Copyright (C) 2013-2018 Alexey Khokholov (Nuke.YKT)
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
 //
 //  Nuked OPL3 emulator.
 //  Thanks:
@@ -11,10 +20,16 @@
 //          Tremolo and phase generator calculation information.
 //      OPLx decapsulated(Matthew Gambrell, Olli Niemitalo):
 //          OPL2 ROMs.
+//      siliconpr0n.org(John McMaster, digshadow):
+//          YMF262 and VRC VII decaps and die shots.
+//
+// version: 1.8
 //
 
 #ifndef NUKEDOPL_H
 #define NUKEDOPL_H
+#define OPL_WRITEBUF_SIZE   1024
+#define OPL_WRITEBUF_DELAY  1
 
 //#include "dosbox.h"
 #include <stdint.h>
@@ -26,10 +41,12 @@ typedef int16_t  Bit16s;
 typedef uint16_t Bit16u;
 typedef int32_t  Bit32s;
 typedef uint32_t Bit32u;
+typedef int64_t  Bit64s;
+typedef uint64_t Bit64u;
 
-struct opl3_chip;
 struct opl3_slot;
 struct opl3_channel;
+struct opl3_chip;
 
 struct opl3_slot {
     opl3_channel *channel;
@@ -57,8 +74,10 @@ struct opl3_slot {
     Bit8u reg_rr;
     Bit8u reg_wf;
     Bit8u key;
+    Bit32u pg_reset;
     Bit32u pg_phase;
-    Bit32u timer;
+    Bit16u pg_phase_out;
+    Bit8u slot_num;
 };
 
 struct opl3_channel {
@@ -74,12 +93,23 @@ struct opl3_channel {
     Bit8u alg;
     Bit8u ksv;
     Bit16u cha, chb;
+    Bit8u ch_num;
+};
+
+struct opl3_writebuf {
+    Bit64u time;
+    Bit16u reg;
+    Bit8u data;
 };
 
 struct opl3_chip {
     opl3_channel channel[18];
     opl3_slot slot[36];
     Bit16u timer;
+    Bit64u eg_timer;
+    Bit8u eg_timerrem;
+    Bit8u eg_state;
+    Bit8u eg_add;
     Bit8u newm;
     Bit8u nts;
     Bit8u rhy;
@@ -91,11 +121,23 @@ struct opl3_chip {
     Bit32u noise;
     Bit16s zeromod;
     Bit32s mixbuff[2];
-
+    Bit8u rm_hh_bit2;
+    Bit8u rm_hh_bit3;
+    Bit8u rm_hh_bit7;
+    Bit8u rm_hh_bit8;
+    Bit8u rm_tc_bit3;
+    Bit8u rm_tc_bit5;
+    //OPL3L
     Bit32s rateratio;
     Bit32s samplecnt;
     Bit16s oldsamples[2];
     Bit16s samples[2];
+
+    Bit64u writebuf_samplecnt;
+    Bit32u writebuf_cur;
+    Bit32u writebuf_last;
+    Bit64u writebuf_lasttime;
+    opl3_writebuf writebuf[OPL_WRITEBUF_SIZE];
 };
 
 void OPL3_Generate(opl3_chip *chip, Bit16s *buf);
@@ -103,5 +145,6 @@ void OPL3_GenerateResampled(opl3_chip *chip, Bit16s *buf);
 void OPL3_Reset(opl3_chip *chip, Bit32u samplerate);
 Bit32u OPL3_WriteAddr(opl3_chip *chip, Bit32u port, Bit8u val);
 void OPL3_WriteReg(opl3_chip *chip, Bit16u reg, Bit8u v);
+void OPL3_WriteRegBuffered(opl3_chip *chip, Bit16u reg, Bit8u v);
 void OPL3_GenerateStream(opl3_chip *chip, Bit16s *sndptr, Bit32u numsamples);
 #endif
