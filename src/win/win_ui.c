40ae8dfe0ee27125ad3bfc0002dfb984c6c9282e
OBattler <oubattler@gmail.com>
2019-10-31 18:15:44 +0100

    Closing the emulator with a window message now asks for confirmation as well.

--- src/win/win_ui.c
+++ src/win/win_ui.c
@@ -8,15 +8,15 @@
  *
  *		user Interface module for WinAPI on Windows.
  *
- * Version:	@(#)win_ui.c	1.0.39	2019/3/20
+ * Version:	@(#)win_ui.c	1.0.40	2019/10/31
  *
  * Authors:	Sarah Walker, <http://pcem-emulator.co.uk/>
  *		Miran Grca, <mgrca8@gmail.com>
  *		Fred N. van Kempen, <decwiz@yahoo.com>
  *
- *		Copyright 2008-2018 Sarah Walker.
- *		Copyright 2016-2018 Miran Grca.
- *		Copyright 2017,2018 Fred N. van Kempen.
+ *		Copyright 2008-2019 Sarah Walker.
+ *		Copyright 2016-2019 Miran Grca.
+ *		Copyright 2017-2019 Fred N. van Kempen.
  *		Copyright 2019 GH Cao.
  */
 #define UNICODE
@@ -697,9 +697,12 @@ MainWindowProcedure(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 		break;
 
 	case WM_SHUTDOWN:
-		UnhookWindowsHookEx(hKeyboardHook);
-		KillTimer(hwnd, TIMER_1SEC);
-		PostQuitMessage(0);
+		i = ui_msgbox(MBX_QUESTION_YN, (wchar_t *)IDS_2122);
+		if (i == 0) {
+			UnhookWindowsHookEx(hKeyboardHook);
+			KillTimer(hwnd, TIMER_1SEC);
+			PostQuitMessage(0);
+		}
 		break;
 
 	case WM_CTRLALTDEL:
