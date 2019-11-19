3cc320d2b20c3a22154d304a6330dca250e3e41b
OBattler <oubattler@gmail.com>
2019-11-19 07:48:49 +0100

    Increased maximum ESDI sectors 99 and fixed the cylinders truncation in config.c.

--- src/win/win_settings.c
+++ src/win/win_settings.c
@@ -8,7 +8,7 @@
  *
  *		Windows 86Box Settings dialog handler.
  *
- * Version:	@(#)win_settings.c	1.0.61	2019/11/19
+ * Version:	@(#)win_settings.c	1.0.62	2019/11/19
  *
  * Authors:	Miran Grca, <mgrca8@gmail.com>
  * 		David Hrdlička, <hrdlickadavid@outlook.com>
@@ -3061,15 +3061,15 @@ hdd_add_file_open_error:
 						max_hpc = 15;
 						max_tracks = 1023;
 						break;
-					case HDD_BUS_ESDI:
-						max_spt = 43;	/* ESDI drives usually had 32 to 43 sectors per track. */
-						max_hpc = 16;
-						max_tracks = 266305;
-						break;
 					case HDD_BUS_XTA:
 						max_spt = 63;
 						max_hpc = 16;
 						max_tracks = 1023;
+						break;
+					case HDD_BUS_ESDI:
+						max_spt = 99;	/* ESDI drives usually had 32 to 43 sectors per track. */
+						max_hpc = 16;
+						max_tracks = 266305;
 						break;
 					case HDD_BUS_IDE:
 						max_spt = 63;
