9db4beeffcb56b6172bf8f002b4d63422d35f5e1
RichardG867 <richardg867@gmail.com>
2020-04-18 00:46:02 -0300

    Add P2B-LS and P3B-F coreboot boards

--- src/machine/m_at_slot1.c
+++ src/machine/m_at_slot1.c
@@ -105,8 +105,12 @@ machine_at_p2bls_init(const machine_t *model)
 {
     int ret;
 
-    ret = bios_load_linear(L"roms/machines/p2bls/1014ls.003",
-			   0x000c0000, 262144, 0);
+    if (strstr(model->internal_name, "_cb"))
+    	ret = bios_load_linear(L"roms/machines/p2bls/coreboot.rom",
+			       0x000c0000, 262144, 0);
+    else
+    	ret = bios_load_linear(L"roms/machines/p2bls/1014ls.003",
+			       0x000c0000, 262144, 0);
 
     if (bios_only || !ret)
 	return ret;
@@ -165,8 +169,12 @@ machine_at_p3bf_init(const machine_t *model)
 {
     int ret;
 
-    ret = bios_load_linear(L"roms/machines/p3bf/bx3f1006.awd",
-			   0x000c0000, 262144, 0);
+    if (strstr(model->internal_name, "_cb"))
+    	ret = bios_load_linear(L"roms/machines/p3bf/coreboot.rom",
+			       0x000c0000, 262144, 0);
+    else
+    	ret = bios_load_linear(L"roms/machines/p3bf/bx3f1006.awd",
+			       0x000c0000, 262144, 0);
 
     if (bios_only || !ret)
 	return ret;
