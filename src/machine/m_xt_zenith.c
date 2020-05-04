a320da78165277e1068de2a9e09bf6fe4d3e1155
daviunic <simunic.david@outlook.com>
2020-05-04 19:54:16 +0200

    Added Zenith Z-150 machine
    Needs more work (DIP switches and jumpers emulation).

--- src/machine/m_xt_zenith.c
+++ src/machine/m_xt_zenith.c
@@ -130,3 +130,24 @@ machine_xt_zenith_init(const machine_t *model)
 
     return ret;
 }
+
+int
+machine_xt_z150_init(const machine_t *model)
+{
+	int ret;
+
+    ret = bios_load_linear(L"roms/machines/z150/BIOS-Z150-1988.bin",
+			   0x000f8000, 32768, 0);
+
+    if (bios_only || !ret)
+	return ret;
+
+	device_add(&keyboard_pc82_device);
+	device_add(&zenith_scratchpad_device);
+	machine_common_init(model);
+    pit_ctr_set_out_func(&pit->counters[1], pit_refresh_timer_xt);
+    device_add(&fdc_xt_device);
+    nmi_init();
+	
+    return ret;
+}
