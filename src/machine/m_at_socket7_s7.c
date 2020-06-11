a260f2913e97fc06553ed1a76858bf642e8cd7a9
OBattler <oubattler@gmail.com>
2020-06-11 13:40:23 +0200

    Fixed the PCI slots on the HP Brio.

--- src/machine/m_at_socket7_s7.c
+++ src/machine/m_at_socket7_s7.c
@@ -572,10 +572,10 @@ machine_at_brio80xx_init(const machine_t *model)
 
     pci_init(PCI_CONFIG_TYPE_1);
     pci_register_slot(0x07, PCI_CARD_SOUTHBRIDGE, 0, 0, 0, 0);
-    pci_register_slot(0x14, PCI_CARD_NORMAL, 4, 1, 2, 3);
-    pci_register_slot(0x11, PCI_CARD_NORMAL, 3, 4, 1, 2);
-    pci_register_slot(0x12, PCI_CARD_NORMAL, 2, 3, 4, 1);
+    pci_register_slot(0x14, PCI_CARD_ONBOARD, 4, 0, 0, 0);
     pci_register_slot(0x13, PCI_CARD_NORMAL, 1, 2, 3, 4);
+    pci_register_slot(0x12, PCI_CARD_NORMAL, 2, 3, 4, 1);
+    pci_register_slot(0x11, PCI_CARD_NORMAL, 3, 4, 1, 2);
     pci_register_slot(0x00, PCI_CARD_NORTHBRIDGE, 0, 0, 0, 0);
     device_add(&i430vx_device);
     device_add(&piix3_device);
