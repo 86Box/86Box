a7cdbc50c01e6c2053d2c6bb446e4dc738d45c0b
tiseno100 <58827426+tiseno100@users.noreply.github.com>
2020-06-15 11:38:00 +0300

    Disable CPU logging

--- src/cpu/cpu.c
+++ src/cpu/cpu.c
@@ -61,7 +61,6 @@
 #endif
 
 /*#define ENABLE_CPU_LOG 1*/
-#define ENABLE_CPU_LOG 1
 
 static void	cpu_write(uint16_t addr, uint8_t val, void *priv);
 static uint8_t	cpu_read(uint16_t addr, void *priv);
