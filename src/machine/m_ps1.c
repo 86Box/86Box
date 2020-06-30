602cadf8630af35a8f4ff76aa011671c6c4fe092
OBattler <oubattler@gmail.com>
2020-06-30 15:52:47 +0200

    Removed the fdc_at_device adding from the PS/1 Model 2133, as the FDC is now correctly added by the Super I/O chip.

--- src/machine/m_ps1.c
+++ src/machine/m_ps1.c
@@ -571,7 +571,6 @@ machine_ps1_m2133_init(const machine_t *model)
 	return ret;
 
     ps1_common_init(model);
-    device_add(&fdc_at_device);
     device_add(&ide_isa_device);
     device_add(&vl82c480_device);
     device_add(&pc87332_ps1_device);
