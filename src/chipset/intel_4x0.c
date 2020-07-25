e1b07f2feef2fd53e0675084b312e0d05c99338c
RichardG867 <richardg867@gmail.com>
2020-07-25 12:28:13 -0300

    Change special 440BX VPC behavior to is_vpc

--- src/chipset/intel_4x0.c
+++ src/chipset/intel_4x0.c
@@ -443,10 +443,8 @@ i4x0_write(int func, int addr, uint8_t val, void *priv)
 				break;
 			case INTEL_440BX: case INTEL_440ZX:
 				regs[0x51] = (regs[0x50] & 0x70) | (val & 0x8f);
-#if defined(DEV_BRANCH) && defined(USE_VIRTUALPC)
-				if (!strcmp(machines[machine].internal_name, "vpc2007"))
+				if (is_vpc)
 					regs[0x51] |= 0x10; /* Virtual PC 2007 BIOS requires a reserved bus speed bit to be set */
-#endif
 				break;
 			case INTEL_440GX:
             			regs[0x51] = (regs[0x50] & 0x88) | (val & 0x08);
@@ -1556,10 +1554,8 @@ static void
 			regs[0x51] |= 0x20;
 		else if ((cpu_busspeed > 66666667) && (cpu_busspeed <= 100000000))
 			regs[0x51] |= 0x00;
-#if defined(DEV_BRANCH) && defined(USE_VIRTUALPC)
-		if (!strcmp(machines[machine].internal_name, "vpc2007"))
+		if (is_vpc)
 			regs[0x51] |= 0x10; /* Virtual PC 2007 BIOS requires a reserved bus speed bit to be set */
-#endif
 		regs[0x57] = 0x28;	/* 4 DIMMs, SDRAM */
 		regs[0x58] = 0x03;
 		regs[0x60] = regs[0x61] = regs[0x62] = regs[0x63] = regs[0x64] = regs[0x65] = regs[0x66] = regs[0x67] = 0x01;
