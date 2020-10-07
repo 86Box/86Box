8933c731b24293c881b6a9ce6e798cad09469423
anabate123 <62159176+anabate123@users.noreply.github.com>
2020-10-06 20:52:06 -0400

    Revert "Update m_at_socket8.c"
    This reverts commit 5e2483d7150164971bce28522dbeb4d75cbdc7f9.

--- src/machine/m_at_socket8.c
+++ src/machine/m_at_socket8.c
@@ -161,41 +161,6 @@ machine_at_vs440fx_init(const machine_t *model)
 }
 
 int
-machine_at_gw2kvs_init(const machine_t *model)
-{
-    int ret;
-
-    ret = bios_load_linear_combined2(L"roms/machines/gw2kvs/1011CS1T.bio",
-				     L"roms/machines/gw2kvs/1011CS1T.bi1",
-				     L"roms/machines/gw2kvs/1011CS1T.bi2",
-				     L"roms/machines/gw2kvs/1011CS1T.bi3",
-				     L"roms/machines/gw2kvs/1011CS1T.rcv",
-				     0x3a000, 128);
-
-    if (bios_only || !ret)
-	return ret;
-
-    machine_at_common_init(model);
-
-    pci_init(PCI_CONFIG_TYPE_1);
-    pci_register_slot(0x00, PCI_CARD_NORTHBRIDGE, 0, 0, 0, 0);
-    pci_register_slot(0x0B, PCI_CARD_NORMAL, 1, 2, 3, 4);
-    pci_register_slot(0x0F, PCI_CARD_NORMAL, 4, 1, 2, 3);
-    pci_register_slot(0x11, PCI_CARD_NORMAL, 3, 4, 1, 2);
-    pci_register_slot(0x13, PCI_CARD_NORMAL, 2, 3, 4, 1);
-    pci_register_slot(0x07, PCI_CARD_SOUTHBRIDGE, 0, 0, 0, 0);
-    device_add(&i440fx_device);
-    device_add(&piix3_device);
-    device_add(&keyboard_ps2_intel_ami_pci_device);
-    device_add(&pc87307_device);
-
-    device_add(&intel_flash_bxt_ami_device);
-
-    return ret;
-}
-
-
-int
 machine_at_ap440fx_init(const machine_t *model)
 {
     int ret;
