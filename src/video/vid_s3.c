2c0a7113c39e46e4f9f890600d58344d9842600b
OBattler <oubattler@gmail.com>
2020-12-09 13:23:37 +0100

    Fixed the new MMIO mapping size of the S3 Virge968, Paintbrush no longer hangs but does not display correctly, pending the implementation of the Video engine.

--- src/video/vid_s3.c
+++ src/video/vid_s3.c
@@ -2709,7 +2709,7 @@ s3_updatemapping(s3_t *s3)
 		/* New MMIO. */
 		if (svga->crtc[0x53] & 0x08) {
 			pclog("New MMIO enabled at %08x, CRTC59 = %02x\n", s3->linear_base + 0x1000000, svga->crtc[0x59]);
-			mem_mapping_set_addr(&s3->new_mmio_mapping, s3->linear_base + 0x1000000, 0x10000);
+			mem_mapping_set_addr(&s3->new_mmio_mapping, s3->linear_base + 0x1000000, 0x20000);
 		} else {
 			pclog("New MMIO disabled\n", s3->linear_base);
 			mem_mapping_disable(&s3->new_mmio_mapping);
