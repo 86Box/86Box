a903e24702a9751dc981c5ba55c201fec3febf73
Cacodemon345 <wahil1976@outlook.com>
2021-08-25 13:45:21 +0600

    Fix remaining warnings

--- src/codegen/codegen_x86-64.c
+++ src/codegen/codegen_x86-64.c
@@ -2,6 +2,7 @@
 
 #include <stdarg.h>
 #include <stdio.h>
+#include <string.h>
 #include <stdint.h>
 #include <stdlib.h>
 #define HAVE_STDARG_H
--- src/include/86box/86box.h
+++ src/include/86box/86box.h
@@ -45,6 +45,13 @@
 # define ENABLE_LOG_COMMANDS	1
 #endif
 
+#ifdef MIN
+#undef MIN
+#endif
+#ifdef MAX
+#undef MAX
+#endif
+
 #define MIN(a, b)	((a) < (b) ? (a) : (b))
 #define MAX(a, b)	((a) > (b) ? (a) : (b))
 #define ABS(x)		((x) > 0 ? (x) : -(x))
