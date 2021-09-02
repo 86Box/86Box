586066d891669e9534e2eab7418a4b66276ee8bb
OBattler <oubattler@gmail.com>
2021-09-02 15:24:17 +0200

    Made NMI's always auto-clear, will revert if it makes things worse.

--- src/cpu/386_dynarec.c
+++ src/cpu/386_dynarec.c
@@ -797,10 +797,14 @@ exec386_dynarec(int cycs)
 			cpu_state.oldpc = cpu_state.pc;
 			x86_int(2);
 			nmi_enable = 0;
+#ifdef OLD_NMI_BEHAVIOR
 			if (nmi_auto_clear) {
 				nmi_auto_clear = 0;
 				nmi = 0;
 			}
+#else
+			nmi = 0;
+#endif
 		} else if ((cpu_state.flags & I_FLAG) && pic.int_pending) {
 			vector = picinterrupt();
 			if (vector != -1) {
