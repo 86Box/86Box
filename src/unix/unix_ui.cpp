cb87c1228a5c235aa6c10bf7e611f0b1f206bc5b
Cacodemon345 <wahil1976@outlook.com>
2021-09-05 01:28:40 +0600

    Implement ZIP and MO menu

--- src/unix/unix_ui.cpp
+++ src/unix/unix_ui.cpp
@@ -15,6 +15,9 @@
 #include <86box/fdd_86f.h>
 #include <86box/cdrom.h>
 #include <86box/scsi.h>
+#include <86box/scsi_device.h>
+#include <86box/mo.h>
+#include <86box/zip.h>
 #include <86box/hdc.h>
 #include <86box/plat.h>
 #include <86box/plat_dynld.h>
@@ -58,6 +61,30 @@ is_valid_cdrom(int i)
 	(scsi_card_current[2] == 0) && (scsi_card_current[3] == 0))
 	return 0;
     return cdrom[i].bus_type != 0;
+}
+
+static inline int
+is_valid_zip(int i)
+{
+    if ((zip_drives[i].bus_type == ZIP_BUS_ATAPI) && !MACHINE_HAS_IDE && memcmp(hdc_get_internal_name(hdc_current), "ide", 3))
+	return 0;
+    if ((zip_drives[i].bus_type == ZIP_BUS_SCSI) && !MACHINE_HAS_SCSI &&
+	(scsi_card_current[0] == 0) && (scsi_card_current[1] == 0) &&
+	(scsi_card_current[2] == 0) && (scsi_card_current[3] == 0))
+	return 0;
+    return zip_drives[i].bus_type != 0;
+}
+
+static inline int
+is_valid_mo(int i)
+{
+    if ((mo_drives[i].bus_type == MO_BUS_ATAPI) && !MACHINE_HAS_IDE && memcmp(hdc_get_internal_name(hdc_current), "ide", 3))
+	return 0;
+    if ((mo_drives[i].bus_type == MO_BUS_SCSI) && !MACHINE_HAS_SCSI &&
+	(scsi_card_current[0] == 0) && (scsi_card_current[1] == 0) &&
+	(scsi_card_current[2] == 0) && (scsi_card_current[3] == 0))
+	return 0;
+    return mo_drives[i].bus_type != 0;
 }
 
 static bool OpenFileChooser(char* res, size_t n)
@@ -198,9 +225,116 @@ struct CDMenu
     }
 };
 
+struct ZIPMenu
+{
+    int zipid;
+    ZIPMenu(int id)
+    {
+        zipid = id;
+    }
+    void RenderImGuiMenu()
+    {
+        std::string str = "ZIP ";
+        str += std::to_string(zip_drives[zipid].is_250 ? 250 : 100);
+        str += " ";
+        str += std::to_string(zipid + 1);
+        str += " (";
+        str += zip_drives[zipid].bus_type == 1 ? "SCSI" : "ATAPI";
+        str += ") ";
+        str += strlen(zip_drives[zipid].image_path) == 0 ? "(empty)" : zip_drives[zipid].image_path;
+        if (ImGui::BeginMenu(str.c_str()))
+        {
+            if (ImGui::MenuItem("Image..."))
+            {
+                char res[4096];
+                if (OpenFileChooser(res, sizeof(res)))
+                {
+                    zip_mount(zipid, res, 0);
+                }
+            }
+            if (ImGui::MenuItem("Image... (write-protected)"))
+            {
+                char res[4096];
+                if (OpenFileChooser(res, sizeof(res)))
+                {
+                    zip_mount(zipid, res, 1);
+                }
+            }
+            if (ImGui::MenuItem("Reload previous image"))
+            {
+                zip_reload(zipid);
+            }
+            if (ImGui::MenuItem("Eject"))
+            {
+                zip_eject(zipid);
+            }
+            ImGui::EndMenu();
+        }
+    }
+};
+
+struct MOMenu
+{
+    int moid;
+    MOMenu(int id)
+    {
+        moid = id;
+    }
+    void RenderImGuiMenu()
+    {
+        std::string str = "MO ";
+        str += std::to_string(moid + 1);
+        str += " (";
+        switch(mo_drives[moid].bus_type)
+        {
+            case MO_BUS_ATAPI:
+                str += "ATAPI";
+                break;
+            case MO_BUS_SCSI:
+                str += "SCSI";
+                break;
+            case MO_BUS_USB:
+                str += "USB";
+                break;
+        }
+        str += ") ";
+        str += strlen(mo_drives[moid].image_path) == 0 ? "(empty)" : mo_drives[moid].image_path;
+        if (ImGui::BeginMenu(str.c_str()))
+        {
+            if (ImGui::MenuItem("Image..."))
+            {
+                char res[4096];
+                if (OpenFileChooser(res, sizeof(res)))
+                {
+                    mo_mount(moid, res, 0);
+                }
+            }
+            if (ImGui::MenuItem("Image... (write-protected)"))
+            {
+                char res[4096];
+                if (OpenFileChooser(res, sizeof(res)))
+                {
+                    mo_mount(moid, res, 1);
+                }
+            }
+            if (ImGui::MenuItem("Reload previous image"))
+            {
+                mo_reload(moid);
+            }
+            if (ImGui::MenuItem("Eject"))
+            {
+                mo_eject(moid);
+            }
+            ImGui::EndMenu();
+        }
+    }
+};
+
 std::vector<CartMenu> cmenu;
 std::vector<FloppyMenu> fddmenu;
 std::vector<CDMenu> cdmenu;
+std::vector<ZIPMenu> zipmenu;
+std::vector<MOMenu> momenu;
 
 extern "C" void
 media_menu_reset()
@@ -221,6 +355,18 @@ media_menu_reset()
     for(int i = 0; i < CDROM_NUM; i++) {
 	if(is_valid_cdrom(i)) {
 		cdmenu.emplace_back(i);
+	}
+	curr++;
+    }
+    for(int i = 0; i < ZIP_NUM; i++) {
+	if(is_valid_zip(i)) {
+		zipmenu.emplace_back(i);
+	}
+	curr++;
+    }
+    for(int i = 0; i < MO_NUM; i++) {
+	if(is_valid_mo(i)) {
+		momenu.emplace_back(i);
 	}
 	curr++;
     }
@@ -310,6 +456,14 @@ extern "C" void RenderImGui()
             for (auto &curcdmenu : cdmenu)
             {
                 curcdmenu.RenderImGuiMenu();
+            }
+            for (auto &curzipmenu : zipmenu)
+            {
+                curzipmenu.RenderImGuiMenu();
+            }
+            for (auto &curmomenu : momenu)
+            {
+                curmomenu.RenderImGuiMenu();
             }
             ImGui::EndMenu();
         }
