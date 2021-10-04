63d208182aea3dadbc0675b36407c9cface83bd4
OBattler <oubattler@gmail.com>
2021-10-04 15:38:43 +0200

    Fixed pic_set_pci().

--- src/pic.c
+++ src/pic.c
@@ -520,7 +520,7 @@ pic_set_pci(void)
 {
     int i;
 
-    for (i = 0x0024; i < 0x0040; i++) {
+    for (i = 0x0024; i < 0x0040; i += 4) {
 	io_sethandler(i, 0x0002, pic_read, NULL, NULL, pic_write, NULL, NULL, &pic);
 	io_sethandler(i + 0x0080, 0x0002, pic_read, NULL, NULL, pic_write, NULL, NULL, &pic2);
     }
