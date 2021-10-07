f6674d259ac0d0fe4a4329695bb103f2fd067fd2
Cacodemon345 <wahil1976@outlook.com>
2021-10-07 12:15:48 +0600

    Move back screenshotting to at blit time

--- src/unix/unix_sdl.c
+++ src/unix/unix_sdl.c
@@ -187,6 +187,10 @@ sdl_blit_shim(int x, int y, int w, int h)
     params.h = h;
     if (!(!sdl_enabled || (x < 0) || (y < 0) || (w <= 0) || (h <= 0) || (w > 2048) || (h > 2048) || (buffer32 == NULL) || (sdl_render == NULL) || (sdl_tex == NULL)))
 	video_copy(interpixels, &(buffer32->line[y][x]), h * (2048 + 64) * sizeof(uint32_t));
+    if (screenshots)
+    {
+	    video_screenshot(interpixels, 0, 0, (2048 + 64));
+    }
     blitreq = 1;
     video_blit_complete();
 }
@@ -252,8 +256,6 @@ sdl_blit(int x, int y, int w, int h)
     r_src.w = w;
     r_src.h = h;
     SDL_UpdateTexture(sdl_tex, &r_src, interpixels, (2048 + 64) * 4);
-    if (screenshots)
-	video_screenshot(interpixels, 0, 0, (2048 + 64));
     blitreq = 0;
 
     sdl_real_blit(&r_src);
