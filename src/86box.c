6bcc8c6ca041d881fb4f1f5051600c9430f798f9
dob205 <dominik.babic0@gmail.com>
2021-11-19 18:04:30 +0100

    Cleaning up things

--- src/86box.c
+++ src/86box.c
@@ -18,6 +18,7 @@
  *		Copyright 2016-2020 Miran Grca.
  *		Copyright 2017-2020 Fred N. van Kempen.
  *		Copyright 2021 Laci bá'
+ *		Copyright 2021 dob205
  */
 #include <inttypes.h>
 #include <stdarg.h>
@@ -418,9 +419,6 @@ pc_init(int argc, char *argv[])
 	 */
 	plat_getcwd(usr_path, sizeof(usr_path) - 1);
 	plat_getcwd(rom_path, sizeof(rom_path) - 1);
-
-	printf("JV:usr_path %s\n",usr_path);
-	printf("JV:rom_path %s\n",usr_path);
 	
 	memset(path, 0x00, sizeof(path));
 	memset(path2, 0x00, sizeof(path));
