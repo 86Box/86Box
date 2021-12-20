3f576ce6cab525e555cbda55221f7714a25d9e8e
David Hrdlička <dhrdlicka@users.noreply.github.com>
2021-12-20 21:23:54 +0100

    Allow overriding the toolchain flags

--- cmake/flags-gcc-armv7.cmake
+++ cmake/flags-gcc-armv7.cmake
@@ -1,4 +1,4 @@
 include(${CMAKE_CURRENT_LIST_DIR}/flags-gcc.cmake)
 
-set(CMAKE_C_FLAGS	"-march=armv7-a -mfloat-abi=hard ${CMAKE_C_FLAGS}")
-set(CMAKE_CXX_FLAGS	"-march=armv7-a -mfloat-abi=hard ${CMAKE_CXX_FLAGS}")
+set(CMAKE_C_FLAGS_INIT      "-march=armv7-a -mfloat-abi=hard ${CMAKE_C_FLAGS_INIT}")
+set(CMAKE_CXX_FLAGS_INIT    "-march=armv7-a -mfloat-abi=hard ${CMAKE_CXX_FLAGS_INIT}")
--- cmake/flags-gcc-x86_64.cmake
+++ cmake/flags-gcc-x86_64.cmake
@@ -1,4 +1,4 @@
 include(${CMAKE_CURRENT_LIST_DIR}/flags-gcc.cmake)
 
-set(CMAKE_C_FLAGS	"-m64 -march=x86-64 -msse2 -mfpmath=sse ${CMAKE_C_FLAGS}")
-set(CMAKE_CXX_FLAGS	"-m64 -march=x86-64 -msse2 -mfpmath=sse ${CMAKE_CXX_FLAGS}")
+set(CMAKE_C_FLAGS_INIT      "-m64 -march=x86-64 -msse2 -mfpmath=sse ${CMAKE_C_FLAGS_INIT}")
+set(CMAKE_CXX_FLAGS_INIT    "-m64 -march=x86-64 -msse2 -mfpmath=sse ${CMAKE_CXX_FLAGS_INIT}")
