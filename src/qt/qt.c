a9ecd17f5c3b51cab40dd88772314289e6c62ada
ts-korhonen <teemu.s.korhonen@gmail.com>
2021-12-20 21:54:25 +0200

    Improve clang fixes

--- src/qt/qt.c
+++ src/qt/qt.c
@@ -2,7 +2,7 @@
  * C functionality for Qt platform, where the C equivalent is not easily
  * implemented in Qt
  */
-#ifdef __MINGW32__
+#if !defined(_WIN32) && !defined(__clang__)
 #include <strings.h>
 #endif
 #include <stdint.h>
