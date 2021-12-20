9e3ad3b79fa5fc0357f8f7b936e324b7d37b023f
David Hrdlička <dhrdlicka@users.noreply.github.com>
2021-12-20 21:54:24 +0100

    Add comments to the toolchain files

--- cmake/flags-gcc.cmake
+++ cmake/flags-gcc.cmake
@@ -1,4 +1,19 @@
-set(CMAKE_CONFIGURATION_TYPES	Debug;Release;Optimized)
+#
+# 86Box     A hypervisor and IBM PC system emulator that specializes in
+#           running old operating systems and software designed for IBM
+#           PC systems and compatibles from 1981 through fairly recent
+#           system designs based on the PCI bus.
+#
+#           This file is part of the 86Box distribution.
+#
+#           CMake toolchain file defining GCC compiler flags.
+#
+# Authors:  David Hrdlička, <hrdlickadavid@outlook.com>
+#
+#           Copyright 2021 David Hrdlička.
+#
+
+set(CMAKE_CONFIGURATION_TYPES       Debug;Release;Optimized)
 
 set(CMAKE_C_FLAGS_INIT              "-fomit-frame-pointer -mstackrealign -Wall -fno-strict-aliasing")
 set(CMAKE_CXX_FLAGS_INIT            ${CMAKE_C_FLAGS_INIT})
--- cmake/llvm-win32-aarch64.cmake
+++ cmake/llvm-win32-aarch64.cmake
@@ -6,14 +6,14 @@
 #
 #           This file is part of the 86Box distribution.
 #
-#           CMake toolchain file for Clang on Windows builds (x64/AMD64 target).
+#           CMake toolchain file for Clang on Windows builds (ARM64 target).
 #
 # Authors:  David Hrdlička, <hrdlickadavid@outlook.com>
 #
 #           Copyright 2021 David Hrdlička.
 #
 
-include(${CMAKE_CURRENT_LIST_DIR}/flags-gcc-x86_64.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/flags-gcc-aarch64.cmake)
 
 # Use the GCC-compatible Clang executables in order to use our flags
 set(CMAKE_C_COMPILER    clang)
@@ -22,9 +22,9 @@ set(CMAKE_CXX_COMPILER  clang++)
 # `llvm-rc` is barely usable as of LLVM 13, using MS' rc.exe for now
 set(CMAKE_RC_COMPILER   rc)
 
-set(CMAKE_C_COMPILER_TARGET     x86_64-pc-windows-msvc)
-set(CMAKE_CXX_COMPILER_TARGET   x86_64-pc-windows-msvc)
+set(CMAKE_C_COMPILER_TARGET     aarch64-pc-windows-msvc)
+set(CMAKE_CXX_COMPILER_TARGET   aarch64-pc-windows-msvc)
 
-set(CMAKE_SYSTEM_PROCESSOR AMD64)
+set(CMAKE_SYSTEM_PROCESSOR ARM64)
 
 # TODO: set the vcpkg target triplet perhaps?
