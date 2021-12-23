461aacc358f55cc925b7ff38fc9cce6ca8274879
qeeg <mszoopers@protonmail.com>
2021-12-22 19:46:18 -0600

    whoops

--- src/cpu/cpu.c
+++ src/cpu/cpu.c
@@ -2022,11 +2022,7 @@ cpu_CPUID(void)
 		} else if (EAX == 1) {
 			EAX = CPUID;
 			EBX = ECX = 0;
-<<<<<<< HEAD
-			EDX = CPUID_FPU | CPUID_VME | CPUID_PSE | CPUID_TSC | CPUID_MSR | CPUID_PAE | CPUID_CMPXCHG8B | CPUID_MTRR | CPUID_PGE | CPUID_SEP | CPUID_CMOV;
-=======
 			EDX = CPUID_FPU | CPUID_VME | CPUID_PSE | CPUID_TSC | CPUID_MSR | CPUID_PAE | CPUID_MCE | CPUID_CMPXCHG8B | CPUID_MTRR | CPUID_MCA | CPUID_SEP | CPUID_CMOV;
->>>>>>> 0126d7dbefed83e1dc9684a3fd9c9531c37b8916
 		} else if (EAX == 2) {
 			EAX = 0x00000001;
 			EBX = ECX = 0;
@@ -2044,11 +2040,7 @@ cpu_CPUID(void)
 		} else if (EAX == 1) {
 			EAX = CPUID;
 			EBX = ECX = 0;
-<<<<<<< HEAD
-			EDX = CPUID_FPU | CPUID_VME | CPUID_PSE | CPUID_TSC | CPUID_MSR | CPUID_PAE | CPUID_CMPXCHG8B | CPUID_MMX | CPUID_MTRR | CPUID_PGE | CPUID_SEP | CPUID_CMOV;
-=======
 			EDX = CPUID_FPU | CPUID_VME | CPUID_PSE | CPUID_TSC | CPUID_MSR | CPUID_PAE | CPUID_MCE | CPUID_CMPXCHG8B | CPUID_MMX | CPUID_MTRR | CPUID_MCA | CPUID_SEP | CPUID_CMOV;
->>>>>>> 0126d7dbefed83e1dc9684a3fd9c9531c37b8916
 		} else if (EAX == 2) {
 			EAX = 0x00000001;
 			EBX = ECX = 0;
@@ -2066,29 +2058,7 @@ cpu_CPUID(void)
 		} else if (EAX == 1) {
 			EAX = CPUID;
 			EBX = ECX = 0;
-<<<<<<< HEAD
-			EDX = CPUID_FPU | CPUID_VME | CPUID_PSE | CPUID_TSC | CPUID_MSR | CPUID_PAE | CPUID_CMPXCHG8B | CPUID_MMX | CPUID_MTRR | CPUID_PGE | CPUID_SEP | CPUID_FXSR | CPUID_CMOV;
-		} else if (EAX == 2) {
-			EAX = 0x00000001;
-			EBX = ECX = 0;
-			EDX = 0x00000000;
-		} else
-			EAX = EBX = ECX = EDX = 0;
-		break;
-
-	case CPU_PENTIUM3:
-		if (!EAX) {
-			EAX = 0x00000002;
-			EBX = 0x756e6547;
-			EDX = 0x49656e69;
-			ECX = 0x6c65746e;
-		} else if (EAX == 1) {
-			EAX = CPUID;
-			EBX = ECX = 0;
-			EDX = CPUID_FPU | CPUID_VME | CPUID_PSE | CPUID_TSC | CPUID_MSR | CPUID_PAE | CPUID_CMPXCHG8B | CPUID_MMX | CPUID_MTRR | CPUID_PGE | CPUID_SEP | CPUID_FXSR | CPUID_CMOV | CPUID_SSE;
-=======
 			EDX = CPUID_FPU | CPUID_VME | CPUID_PSE | CPUID_TSC | CPUID_MSR | CPUID_PAE | CPUID_MCE | CPUID_CMPXCHG8B | CPUID_MMX | CPUID_MTRR | CPUID_MCA | CPUID_SEP | CPUID_FXSR | CPUID_CMOV;
->>>>>>> 0126d7dbefed83e1dc9684a3fd9c9531c37b8916
 		} else if (EAX == 2) {
 			EAX = 0x00000001;
 			EBX = ECX = 0;
@@ -2860,15 +2830,6 @@ amd_k_invalid_wrmsr:
 			case 0x10:
 				tsc = EAX | ((uint64_t)EDX << 32);
 				break;
-<<<<<<< HEAD
-			case 0x17:
-				if (cpu_s->cpu_type != CPU_PENTIUM2D && cpu_s->cpu_type != CPU_PENTIUM3)
-					goto i686_invalid_wrmsr;
-
-				msr.ecx17 = EAX | ((uint64_t)EDX << 32);
-				break;
-=======
->>>>>>> 0126d7dbefed83e1dc9684a3fd9c9531c37b8916
 			case 0x1b:
 				cpu_log("APIC_BASE write: %08X%08X\n", EDX, EAX);
 				// msr.apic_base = EAX | ((uint64_t)EDX << 32);
