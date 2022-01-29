fb22e1fc737b18c6fd3977107181415383677fd1
linear cannon <dev@linear.network>
2022-01-29 05:30:34 -0800

    86box.c: pc_reset_hard_init: dont set cycles_main if DYNAREC=Off

--- src/86box.c
+++ src/86box.c
@@ -1046,8 +1046,12 @@ pc_reset_hard_init(void)
 	atfullspeed = 0;
 	pc_full_speed();
 
-	cycles = cycles_main = 0;
-	
+
+	cycles = 0;
+#ifdef USE_DYNAREC
+	cycles_main = 0;
+#endif
+
 	update_mouse_msg();
 }
 
