a637f61d4f9f60f609625965ba5501d126017851
Cacodemon345 <wahil1976@outlook.com>
2022-02-07 15:00:02 +0600

    Add missing copyright notices

--- src/qt/qt_specifydimensions.cpp
+++ src/qt/qt_specifydimensions.cpp
@@ -1,3 +1,19 @@
+/*
+ * 86Box	A hypervisor and IBM PC system emulator that specializes in
+ *		running old operating systems and software designed for IBM
+ *		PC systems and compatibles from 1981 through fairly recent
+ *		system designs based on the PCI bus.
+ *
+ *		This file is part of the 86Box distribution.
+ *
+ *		Specify dimensions UI module.
+ *
+ *
+ *
+ * Authors:	Cacodemon345
+ *
+ *		Copyright 2021-2022 Cacodemon345
+ */
 #include "qt_specifydimensions.h"
 #include "ui_qt_specifydimensions.h"
 
