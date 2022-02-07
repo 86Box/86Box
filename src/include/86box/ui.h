b21d5b5a43a0104e829501084d494c9ac92d3f49
Cacodemon345 <wahil1976@outlook.com>
2022-02-08 01:52:02 +0600

    Add MT-32 LCD message display support in the status bar

--- src/include/86box/ui.h
+++ src/include/86box/ui.h
@@ -74,6 +74,7 @@ extern void	ui_sb_update_icon_state(int tag, int active);
 extern void	ui_sb_set_text_w(wchar_t *wstr);
 extern void	ui_sb_set_text(char *str);
 extern void	ui_sb_bugui(char *str);
+extern void	ui_sb_mt32lcd(char *str);
 
 #ifdef __cplusplus
 }
