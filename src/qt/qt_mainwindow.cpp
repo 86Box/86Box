accd4ca3985475d308831ada3192818ade1bbba6
Cacodemon345 <wahil1976@outlook.com>
2022-02-09 01:30:02 +0600

    qt: Scale statusbar icons based on DPI

--- src/qt/qt_mainwindow.cpp
+++ src/qt/qt_mainwindow.cpp
@@ -417,6 +417,7 @@ MainWindow::MainWindow(QWidget *parent) :
 #endif
 
     ui->toolBar->setIconSize(QSize(16 * screen()->devicePixelRatio(), 16 * screen()->devicePixelRatio()));
+    ui->statusbar->resize(ui->statusbar->width(), ui->statusbar->sizeHint().height() * screen()->devicePixelRatio());
 }
 
 void MainWindow::closeEvent(QCloseEvent *event) {
