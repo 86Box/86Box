775dad5fa3803541df067add30385c5f982fa937
Cacodemon345 <wahil1976@outlook.com>
2022-02-10 15:30:39 +0600

    qt: Use QDesktopWidget for screen retrival

--- src/qt/qt_util.hpp
+++ src/qt/qt_util.hpp
@@ -2,13 +2,17 @@
 #define QT_UTIL_HPP
 
 #include <QString>
+#include <QWidget>
 
 #include <initializer_list>
 
+class QScreen;
 namespace util
 {
     /* Creates extension list for qt filedialog */
     QString DlgFilter(std::initializer_list<QString> extensions, bool last = false);
+    /* Returns screen the widget is on */
+    QScreen* screenOfWidget(QWidget* widget);
 };
 
 #endif
