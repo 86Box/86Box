ee5630f9c4c5d570ea51b36fc4aaf54c306cc0c8
OBattler <oubattler@gmail.com>
2022-02-18 17:51:00 +0100

    Updated the version number to 3.3.

--- src/include_make/86box/version.h
+++ src/include_make/86box/version.h
@@ -20,12 +20,12 @@
 #define EMU_NAME	"86Box"
 #define EMU_NAME_W	LSTR(EMU_NAME)
 
-#define EMU_VERSION	"3.2.1"
+#define EMU_VERSION	"3.3"
 #define EMU_VERSION_W	LSTR(EMU_VERSION)
-#define EMU_VERSION_EX	"3.21"
+#define EMU_VERSION_EX	"3.30"
 #define EMU_VERSION_MAJ	3
-#define EMU_VERSION_MIN	2
-#define EMU_VERSION_PATCH 1
+#define EMU_VERSION_MIN	3
+#define EMU_VERSION_PATCH 0
 
 #define EMU_BUILD_NUM	0
 
