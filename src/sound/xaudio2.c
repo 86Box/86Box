3101d74658d1e65e83abf86de4f4170b6a2fbe10
David Hrdlička <dhrdlicka@users.noreply.github.com>
2022-02-20 00:41:28 +0100

    Add XAudio2 support to the FAudio sound backend

--- src/sound/xaudio2.c
+++ src/sound/xaudio2.c
@@ -6,7 +6,7 @@
  *
  *		This file is part of the 86Box distribution.
  *
- *		Interface to the FAudio audio processing library.
+ *		Interface to the XAudio2 audio processing library.
  *
  *
  *
@@ -20,95 +20,152 @@
 #include <string.h>
 #include <stdlib.h>
 #include <wchar.h>
-#include <FAudio.h>
 
-extern "C"
-{
+#if defined(_WIN32) && !defined(USE_FAUDIO)
+#define COBJMACROS
+#include <xaudio2.h>
+#else
+#include <FAudio.h>
+#include <FAudio_compat.h>
+#endif
+
 #include <86box/86box.h>
 #include <86box/sound.h>
 #include <86box/midi.h>
+#include <86box/plat_dynld.h>
+
+#if defined(_WIN32) && !defined(USE_FAUDIO)
+static void *xaudio2_handle = NULL;
+static HRESULT (WINAPI *pXAudio2Create)(IXAudio2** ppXAudio2, uint32_t Flags, XAUDIO2_PROCESSOR XAudio2Processor);
+static dllimp_t xaudio2_imports[] = {
+  { "XAudio2Create",	&pXAudio2Create	},
+  { NULL,		NULL		},
+};
+#define XAudio2Create pXAudio2Create
+#endif
 
 static int midi_freq = 44100;
 static int midi_buf_size = 4410;
 static int initialized = 0;
-static FAudio* faudio = nullptr;
-static FAudioMasteringVoice* mastervoice = nullptr;
-static FAudioSourceVoice* srcvoice = nullptr;
-static FAudioSourceVoice* srcvoicemidi = nullptr;
-static FAudioSourceVoice* srcvoicecd = nullptr;
+static IXAudio2 *xaudio2 = NULL;
+static IXAudio2MasteringVoice *mastervoice = NULL;
+static IXAudio2SourceVoice *srcvoice = NULL;
+static IXAudio2SourceVoice *srcvoicemidi = NULL;
+static IXAudio2SourceVoice *srcvoicecd = NULL;
 
 #define FREQ	48000
 #define BUFLEN	SOUNDBUFLEN
 
-static void FAUDIOCALL
-onBufferFinished(
-	FAudioVoiceCallback *callback,
-	void *pBufferContext)
+static void WINAPI OnVoiceProcessingPassStart(IXAudio2VoiceCallback *callback, uint32_t bytesRequired) {}
+static void WINAPI OnVoiceProcessingPassEnd(IXAudio2VoiceCallback *callback) {}
+static void WINAPI OnStreamEnd(IXAudio2VoiceCallback *callback) {}
+static void WINAPI OnBufferStart(IXAudio2VoiceCallback *callback, void *pBufferContext) {}
+static void WINAPI OnLoopEnd(IXAudio2VoiceCallback *callback, void *pBufferContext) {}
+static void WINAPI OnVoiceError(IXAudio2VoiceCallback *callback, void *pBufferContext, HRESULT error) {}
+
+static void WINAPI
+OnBufferEnd(IXAudio2VoiceCallback *callback, void *pBufferContext)
 {
-    if (sound_is_float) delete[] (float*)(pBufferContext);
-    else delete[] (int16_t*)(pBufferContext);
-    
+    free(pBufferContext);
 }
 
-static FAudioVoiceCallback callbacks = 
+#if defined(_WIN32) && !defined(USE_FAUDIO)
+static IXAudio2VoiceCallbackVtbl callbacksVtbl =
+#else
+static FAudioVoiceCallback callbacks =
+#endif
 {
-    onBufferFinished
+    .OnVoiceProcessingPassStart = OnVoiceProcessingPassStart,
+    .OnVoiceProcessingPassEnd = OnVoiceProcessingPassEnd,
+    .OnStreamEnd = OnStreamEnd,
+    .OnBufferStart = OnBufferStart,
+    .OnBufferEnd = OnBufferEnd,
+    .OnLoopEnd = OnLoopEnd,
+    .OnVoiceError = OnVoiceError
 };
+
+#if defined(_WIN32) && !defined(USE_FAUDIO)
+static IXAudio2VoiceCallback callbacks = { &callbacksVtbl };
+#endif
 
 void
 inital()
 {
-    if (FAudioCreate(&faudio, 0, FAUDIO_DEFAULT_PROCESSOR))
+#if defined(_WIN32) && !defined(USE_FAUDIO)
+    if (xaudio2_handle == NULL) {
+	xaudio2_handle = dynld_module("xaudio2_9.dll", xaudio2_imports);
+    }
+
+    if (xaudio2_handle == NULL) {
+	xaudio2_handle = dynld_module("xaudio2_9redist.dll", xaudio2_imports);
+    }
+
+    if (xaudio2_handle == NULL) {
+	return;
+    }
+#endif
+
+    if (XAudio2Create(&xaudio2, 0, XAUDIO2_DEFAULT_PROCESSOR))
     {
         return;
     }
 
-    if (FAudio_CreateMasteringVoice(faudio, &mastervoice, 2, FREQ, 0, 0, nullptr))
+    if (IXAudio2_CreateMasteringVoice(xaudio2, &mastervoice, 2, FREQ, 0, 0, NULL, 0))
     {
-        FAudio_Release(faudio);
-        faudio = nullptr;
+        IXAudio2_Release(xaudio2);
+        xaudio2 = NULL;
         return;
     }
-    FAudioWaveFormatEx fmt;
+
+    WAVEFORMATEX fmt;
     fmt.nChannels = 2;
+
     if (sound_is_float)
     {
-        fmt.wFormatTag = FAUDIO_FORMAT_IEEE_FLOAT;
+        fmt.wFormatTag = WAVE_FORMAT_IEEE_FLOAT;
         fmt.wBitsPerSample = 32;
     }
     else
     {
-        fmt.wFormatTag = FAUDIO_FORMAT_PCM;
+        fmt.wFormatTag = WAVE_FORMAT_PCM;
         fmt.wBitsPerSample = 16;
     }
+
     fmt.nSamplesPerSec = FREQ;
     fmt.nBlockAlign = fmt.nChannels * fmt.wBitsPerSample / 8;
     fmt.nAvgBytesPerSec = fmt.nSamplesPerSec * fmt.nBlockAlign;
     fmt.cbSize = 0;
-    if (FAudio_CreateSourceVoice(faudio, &srcvoice, &fmt, 0, 2.0f, &callbacks, nullptr, nullptr))
+
+    if (IXAudio2_CreateSourceVoice(xaudio2, &srcvoice, &fmt, 0, 2.0f, &callbacks, NULL, NULL))
     {
-        FAudioVoice_DestroyVoice(mastervoice);
-        FAudio_Release(faudio);
-        faudio = nullptr;
-        mastervoice = nullptr;
+        IXAudio2MasteringVoice_DestroyVoice(mastervoice);
+        IXAudio2_Release(xaudio2);
+        xaudio2 = NULL;
+        mastervoice = NULL;
         return;
     }
+
     fmt.nSamplesPerSec = CD_FREQ;
     fmt.nBlockAlign = fmt.nChannels * fmt.wBitsPerSample / 8;
     fmt.nAvgBytesPerSec = fmt.nSamplesPerSec * fmt.nBlockAlign;
-    FAudio_CreateSourceVoice(faudio, &srcvoicecd, &fmt, 0, 2.0f, &callbacks, nullptr, nullptr);
-    FAudioVoice_SetVolume(srcvoice, 1, FAUDIO_COMMIT_NOW);
-    FAudioSourceVoice_Start(srcvoice, 0, FAUDIO_COMMIT_NOW);
-    FAudioSourceVoice_Start(srcvoicecd, 0, FAUDIO_COMMIT_NOW);
-    auto mdn = midi_device_get_internal_name(midi_device_current);
+
+    IXAudio2_CreateSourceVoice(xaudio2, &srcvoicecd, &fmt, 0, 2.0f, &callbacks, NULL, NULL);
+
+    IXAudio2SourceVoice_SetVolume(srcvoice, 1, XAUDIO2_COMMIT_NOW);
+    IXAudio2SourceVoice_Start(srcvoice, 0, XAUDIO2_COMMIT_NOW);
+    IXAudio2SourceVoice_Start(srcvoicecd, 0, XAUDIO2_COMMIT_NOW);
+
+    char *mdn = midi_device_get_internal_name(midi_device_current);
+
     if (strcmp(mdn, "none") && strcmp(mdn, SYSTEM_MIDI_INTERNAL_NAME))
-	{
+    {
         fmt.nSamplesPerSec = midi_freq;
         fmt.nBlockAlign = fmt.nChannels * fmt.wBitsPerSample / 8;
         fmt.nAvgBytesPerSec = fmt.nSamplesPerSec * fmt.nBlockAlign;
-        FAudio_CreateSourceVoice(faudio, &srcvoicemidi, &fmt, 0, 2.0f, &callbacks, nullptr, nullptr);
-        FAudioSourceVoice_Start(srcvoicemidi, 0, FAUDIO_COMMIT_NOW);
+        IXAudio2_CreateSourceVoice(xaudio2, &srcvoicemidi, &fmt, 0, 2.0f, &callbacks, NULL, NULL);
+        IXAudio2SourceVoice_Start(srcvoicemidi, 0, XAUDIO2_COMMIT_NOW);
     }
+
     initialized = 1;
 }
 
@@ -117,52 +174,57 @@ closeal()
 {
     if (!initialized) return;
     initialized = 0;
-    FAudioSourceVoice_Stop(srcvoice, 0, FAUDIO_COMMIT_NOW);
-    FAudioSourceVoice_FlushSourceBuffers(srcvoice);
-    FAudioSourceVoice_Stop(srcvoicecd, 0, FAUDIO_COMMIT_NOW);
-    FAudioSourceVoice_FlushSourceBuffers(srcvoicecd);
+    IXAudio2SourceVoice_Stop(srcvoice, 0, XAUDIO2_COMMIT_NOW);
+    IXAudio2SourceVoice_FlushSourceBuffers(srcvoice);
+    IXAudio2SourceVoice_Stop(srcvoicecd, 0, XAUDIO2_COMMIT_NOW);
+    IXAudio2SourceVoice_FlushSourceBuffers(srcvoicecd);
     if (srcvoicemidi)
     {
-        FAudioSourceVoice_Stop(srcvoicemidi, 0, FAUDIO_COMMIT_NOW);
-        FAudioSourceVoice_FlushSourceBuffers(srcvoicemidi);
-        FAudioVoice_DestroyVoice(srcvoicemidi);
+        IXAudio2SourceVoice_Stop(srcvoicemidi, 0, XAUDIO2_COMMIT_NOW);
+        IXAudio2SourceVoice_FlushSourceBuffers(srcvoicemidi);
+        IXAudio2SourceVoice_DestroyVoice(srcvoicemidi);
     }
-    FAudioVoice_DestroyVoice(srcvoice);
-    FAudioVoice_DestroyVoice(srcvoicecd);
-    FAudioVoice_DestroyVoice(mastervoice);
-    FAudio_Release(faudio);
-    srcvoice = srcvoicecd = srcvoicemidi = nullptr;
-    mastervoice = nullptr;
-    faudio = nullptr;
+    IXAudio2SourceVoice_DestroyVoice(srcvoice);
+    IXAudio2SourceVoice_DestroyVoice(srcvoicecd);
+    IXAudio2MasteringVoice_DestroyVoice(mastervoice);
+    IXAudio2_Release(xaudio2);
+    srcvoice = srcvoicecd = srcvoicemidi = NULL;
+    mastervoice = NULL;
+    xaudio2 = NULL;
+
+#if defined(_WIN32) && !defined(USE_FAUDIO)
+    dynld_close(xaudio2_handle);
+    xaudio2_handle = NULL;
+#endif
 }
 
 void
-givealbuffer_common(void *buf, FAudioSourceVoice* sourcevoice, size_t buflen)
+givealbuffer_common(void *buf, IXAudio2SourceVoice* sourcevoice, size_t buflen)
 {
     if (!initialized) return;
     
-    FAudioVoice_SetVolume(mastervoice, pow(10.0, (double)sound_gain / 20.0), FAUDIO_COMMIT_NOW);
-    FAudioBuffer buffer{};
+    IXAudio2MasteringVoice_SetVolume(mastervoice, pow(10.0, (double)sound_gain / 20.0), XAUDIO2_COMMIT_NOW);
+    XAUDIO2_BUFFER buffer = {0};
     buffer.Flags = 0;
     if (sound_is_float)
     {
-        buffer.pAudioData = (uint8_t*)new float[buflen];
+        buffer.pAudioData = calloc(buflen, sizeof(float));
         buffer.AudioBytes = (buflen) * sizeof(float);
     }
     else
     {
-        buffer.pAudioData = (uint8_t*)new int16_t[buflen];
+        buffer.pAudioData = calloc(buflen, sizeof(int16_t));
         buffer.AudioBytes = (buflen) * sizeof(int16_t);
     }
-    if (buffer.pAudioData == nullptr)
+    if (buffer.pAudioData == NULL)
     {
-        fatal("faudio: Out Of Memory!");
+        fatal("xaudio2: Out Of Memory!");
     }
     memcpy((void*)buffer.pAudioData, buf, buffer.AudioBytes);
     buffer.PlayBegin = buffer.PlayLength = 0;
     buffer.PlayLength = buflen >> 1;
     buffer.pContext = (void*)buffer.pAudioData;
-    FAudioSourceVoice_SubmitSourceBuffer(sourcevoice, &buffer, nullptr);
+    IXAudio2SourceVoice_SubmitSourceBuffer(sourcevoice, &buffer, NULL);
 }
 
 void
@@ -185,28 +247,28 @@ al_set_midi(int freq, int buf_size)
 
     if (initialized && srcvoicemidi)
     {
-        FAudioSourceVoice_Stop(srcvoicemidi, 0, FAUDIO_COMMIT_NOW);
-        FAudioSourceVoice_FlushSourceBuffers(srcvoicemidi);
-        FAudioVoice_DestroyVoice(srcvoicemidi);
-        srcvoicemidi = nullptr;
-        FAudioWaveFormatEx fmt;
+        IXAudio2SourceVoice_Stop(srcvoicemidi, 0, XAUDIO2_COMMIT_NOW);
+        IXAudio2SourceVoice_FlushSourceBuffers(srcvoicemidi);
+        IXAudio2SourceVoice_DestroyVoice(srcvoicemidi);
+        srcvoicemidi = NULL;
+        WAVEFORMATEX fmt;
         fmt.nChannels = 2;
         if (sound_is_float)
         {
-            fmt.wFormatTag = FAUDIO_FORMAT_IEEE_FLOAT;
+            fmt.wFormatTag = WAVE_FORMAT_IEEE_FLOAT;
             fmt.wBitsPerSample = 32;
         }
         else
         {
-            fmt.wFormatTag = FAUDIO_FORMAT_PCM;
+            fmt.wFormatTag = WAVE_FORMAT_PCM;
             fmt.wBitsPerSample = 16;
         }
         fmt.nSamplesPerSec = midi_freq;
         fmt.nBlockAlign = fmt.nChannels * fmt.wBitsPerSample / 8;
         fmt.nAvgBytesPerSec = fmt.nSamplesPerSec * fmt.nBlockAlign;
         fmt.cbSize = 0;
-        FAudio_CreateSourceVoice(faudio, &srcvoicemidi, &fmt, 0, 2.0f, &callbacks, nullptr, nullptr);
-        FAudioSourceVoice_Start(srcvoicemidi, 0, FAUDIO_COMMIT_NOW);
+        IXAudio2_CreateSourceVoice(xaudio2, &srcvoicemidi, &fmt, 0, 2.0f, &callbacks, NULL, NULL);
+        IXAudio2SourceVoice_Start(srcvoicemidi, 0, XAUDIO2_COMMIT_NOW);
     }
 }
 
@@ -214,6 +276,4 @@ void
 givealbuffer_midi(void *buf, uint32_t size)
 {
     givealbuffer_common(buf, srcvoicemidi, size);
-}
-
 }
