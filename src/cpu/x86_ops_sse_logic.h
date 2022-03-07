bc013b3e255770107e504e95242866e12773fe1e
qeeg <mszoopers@protonmail.com>
2022-03-07 15:44:42 -0600

    fix andnps

--- src/cpu/x86_ops_sse_logic.h
+++ src/cpu/x86_ops_sse_logic.h
@@ -63,10 +63,10 @@ static int opANDNPS_q_xmm_a16(uint32_t fetchdat)
         fetch_ea_16(fetchdat);
         if (cpu_mod == 3)
         {
-                XMM[cpu_reg].l[0] &= ~XMM[cpu_rm].l[0];
-                XMM[cpu_reg].l[1] &= ~XMM[cpu_rm].l[1];
-                XMM[cpu_reg].l[2] &= ~XMM[cpu_rm].l[2];
-                XMM[cpu_reg].l[3] &= ~XMM[cpu_rm].l[3];
+                XMM[cpu_reg].l[0] = ~XMM[cpu_reg].l[0] & XMM[cpu_rm].l[0];
+                XMM[cpu_reg].l[1] = ~XMM[cpu_reg].l[1] & XMM[cpu_rm].l[1];
+                XMM[cpu_reg].l[2] = ~XMM[cpu_reg].l[2] & XMM[cpu_rm].l[2];
+                XMM[cpu_reg].l[3] = ~XMM[cpu_reg].l[3] & XMM[cpu_rm].l[3];
                 CLOCK_CYCLES(1);
         }
         else
@@ -78,10 +78,10 @@ static int opANDNPS_q_xmm_a16(uint32_t fetchdat)
                 dst[1] = readmeml(easeg, cpu_state.eaaddr + 4); if (cpu_state.abrt) return 1;
                 dst[2] = readmeml(easeg, cpu_state.eaaddr + 8); if (cpu_state.abrt) return 1;
                 dst[3] = readmeml(easeg, cpu_state.eaaddr + 12); if (cpu_state.abrt) return 1;
-                XMM[cpu_reg].l[0] &= ~dst[0];
-                XMM[cpu_reg].l[1] &= ~dst[1];
-                XMM[cpu_reg].l[2] &= ~dst[2];
-                XMM[cpu_reg].l[3] &= ~dst[3];
+                XMM[cpu_reg].l[0] = ~XMM[cpu_reg].l[0] & dst[0];
+                XMM[cpu_reg].l[1] = ~XMM[cpu_reg].l[0] & dst[1];
+                XMM[cpu_reg].l[2] = ~XMM[cpu_reg].l[0] & dst[2];
+                XMM[cpu_reg].l[3] = ~XMM[cpu_reg].l[0] & dst[3];
 
                 CLOCK_CYCLES(2);
         }
@@ -93,10 +93,10 @@ static int opANDNPS_q_xmm_a32(uint32_t fetchdat)
         fetch_ea_32(fetchdat);
         if (cpu_mod == 3)
         {
-                XMM[cpu_reg].l[0] &= ~XMM[cpu_rm].l[0];
-                XMM[cpu_reg].l[1] &= ~XMM[cpu_rm].l[1];
-                XMM[cpu_reg].l[2] &= ~XMM[cpu_rm].l[2];
-                XMM[cpu_reg].l[3] &= ~XMM[cpu_rm].l[3];
+                XMM[cpu_reg].l[0] = ~XMM[cpu_reg].l[0] & XMM[cpu_rm].l[0];
+                XMM[cpu_reg].l[1] = ~XMM[cpu_reg].l[1] & XMM[cpu_rm].l[1];
+                XMM[cpu_reg].l[2] = ~XMM[cpu_reg].l[2] & XMM[cpu_rm].l[2];
+                XMM[cpu_reg].l[3] = ~XMM[cpu_reg].l[3] & XMM[cpu_rm].l[3];
                 CLOCK_CYCLES(1);
         }
         else
@@ -108,10 +108,10 @@ static int opANDNPS_q_xmm_a32(uint32_t fetchdat)
                 dst[1] = readmeml(easeg, cpu_state.eaaddr + 4); if (cpu_state.abrt) return 1;
                 dst[2] = readmeml(easeg, cpu_state.eaaddr + 8); if (cpu_state.abrt) return 1;
                 dst[3] = readmeml(easeg, cpu_state.eaaddr + 12); if (cpu_state.abrt) return 1;
-                XMM[cpu_reg].l[0] &= ~dst[0];
-                XMM[cpu_reg].l[1] &= ~dst[1];
-                XMM[cpu_reg].l[2] &= ~dst[2];
-                XMM[cpu_reg].l[3] &= ~dst[3];
+                XMM[cpu_reg].l[0] = ~XMM[cpu_reg].l[0] & dst[0];
+                XMM[cpu_reg].l[1] = ~XMM[cpu_reg].l[0] & dst[1];
+                XMM[cpu_reg].l[2] = ~XMM[cpu_reg].l[0] & dst[2];
+                XMM[cpu_reg].l[3] = ~XMM[cpu_reg].l[0] & dst[3];
 
                 CLOCK_CYCLES(2);
         }
