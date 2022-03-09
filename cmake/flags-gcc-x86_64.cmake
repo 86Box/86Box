14f834477a0555affd7feb0cd0b76570d91b8c96
David Hrdlička <dhrdlicka@users.noreply.github.com>
2022-03-09 02:52:46 +0100

    Outline atomics on AArch64, should make us ARMv8.0 compatible

--- cmake/flags-gcc-x86_64.cmake
+++ cmake/flags-gcc-x86_64.cmake
@@ -14,7 +14,7 @@
 #           Copyright 2021 David Hrdlička.
 #
 
-string(APPEND CMAKE_C_FLAGS_INIT    " -m64 -march=x86-64 -msse2 -mfpmath=sse -mstackrealign ${CMAKE_C_FLAGS_INIT}")
-string(APPEND CMAKE_CXX_FLAGS_INIT  " -m64 -march=x86-64 -msse2 -mfpmath=sse -mstackrealign ${CMAKE_CXX_FLAGS_INIT}")
+string(APPEND CMAKE_C_FLAGS_INIT    " -m64 -march=x86-64 -msse2 -mfpmath=sse -mstackrealign")
+string(APPEND CMAKE_CXX_FLAGS_INIT  " -m64 -march=x86-64 -msse2 -mfpmath=sse -mstackrealign")
 
 include(${CMAKE_CURRENT_LIST_DIR}/flags-gcc.cmake)
