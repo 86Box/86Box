105fe47bf3f5e3fb0ed8e693fabcbec8bbbe4194
ts-korhonen <teemu.s.korhonen@gmail.com>
2022-03-12 15:38:44 +0200

    qt: GL core; use safe version of strcpy

--- src/qt/qt_opengloptions.cpp
+++ src/qt/qt_opengloptions.cpp
@@ -14,6 +14,7 @@
  *      Copyright 2022 Teemu Korhonen
  */
 
+#include <QByteArray>
 #include <QFile>
 #include <QRegularExpression>
 #include <QStringBuilder>
@@ -89,7 +90,7 @@ OpenGLOptions::save() const
     auto path = m_shaders.first().path().toLocal8Bit();
 
     if (!path.isEmpty())
-        strcpy(video_shader, path.constData());
+        qstrncpy(video_shader, path.constData(), sizeof(video_shader));
     else
         video_shader[0] = '\0';
 }
