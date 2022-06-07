92c3768a486e5e8c779833764513318634a03664
OBattler <oubattler@gmail.com>
2022-06-07 19:01:50 +0200

    Added two missing checks to the QT renderer.

--- src/qt/qt_rendererstack.cpp
+++ src/qt/qt_rendererstack.cpp
@@ -351,7 +351,7 @@ RendererStack::createRenderer(Renderer renderer)
 void
 RendererStack::blit(int x, int y, int w, int h)
 {
-    if ((w <= 0) || (h <= 0) || (w > 2048) || (h > 2048) || (buffer32 == NULL) || imagebufs.empty() || std::get<std::atomic_flag *>(imagebufs[currentBuf])->test_and_set()) {
+    if ((x < 0) || (y < 0) || (w <= 0) || (h <= 0) || (w > 2048) || (h > 2048) || (buffer32 == NULL) || imagebufs.empty() || std::get<std::atomic_flag *>(imagebufs[currentBuf])->test_and_set()) {
         video_blit_complete();
         return;
     }
