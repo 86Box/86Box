194b85b601380f0201f86e5d6b5d1abf6f6c756f
RichardG867 <richardg867@gmail.com>
2022-08-31 21:46:30 -0300

    agpgart: Fix AGP aperture never being enabled

--- src/video/agpgart.c
+++ src/video/agpgart.c
@@ -51,10 +51,11 @@ agpgart_set_aperture(agpgart_t *dev, uint32_t base, uint32_t size, int enable)
     /* Disable old aperture mapping. */
     mem_mapping_disable(&dev->aperture_mapping);
 
-    /* Set new aperture base address, size and mask. */
+    /* Set new aperture base address, size, mask and enable. */
     dev->aperture_base = base;
     dev->aperture_size = size;
     dev->aperture_mask = size - 1;
+    dev->aperture_enable = enable;
 
     /* Enable new aperture mapping if requested. */
     if (dev->aperture_base && dev->aperture_size && dev->aperture_enable) {
