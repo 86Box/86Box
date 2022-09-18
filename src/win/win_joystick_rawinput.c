740d7af8d61838e2de11cc2508467a6eac8db361
Jasmine Iwanek <jasmine@iwanek.co.uk>
2022-09-18 17:16:00 -0400

    clang-format in src/win/

--- src/win/win_joystick_rawinput.c
+++ src/win/win_joystick_rawinput.c
@@ -219,9 +219,9 @@ end:
 void
 joystick_get_device_name(raw_joystick_t *rawjoy, plat_joystick_t *joy, PRID_DEVICE_INFO info)
 {
-    UINT  size                  = 0;
+    UINT   size                  = 0;
     WCHAR *device_name           = NULL;
-    WCHAR device_desc_wide[200] = { 0 };
+    WCHAR  device_desc_wide[200] = { 0 };
 
     GetRawInputDeviceInfoW(rawjoy->hdevice, RIDI_DEVICENAME, device_name, &size);
     device_name = calloc(size, sizeof(WCHAR));
@@ -229,7 +229,7 @@ joystick_get_device_name(raw_joystick_t *rawjoy, plat_joystick_t *joy, PRID_DEVI
         fatal("joystick_get_capabilities: Failed to get device name.\n");
 
     HANDLE hDevObj = CreateFileW(device_name, GENERIC_READ | GENERIC_WRITE,
-                                FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, 0, NULL);
+                                 FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, 0, NULL);
     if (hDevObj) {
         HidD_GetProductString(hDevObj, device_desc_wide, sizeof(WCHAR) * 200);
         CloseHandle(hDevObj);
