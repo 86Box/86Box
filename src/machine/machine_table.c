f807bb2cccb904feb40e4440fff33b28511ded93
OBattler <oubattler@gmail.com>
2022-11-09 04:32:59 +0100

    It is the M240 that has the internal HDC, not the M24.

--- src/machine/machine_table.c
+++ src/machine/machine_table.c
@@ -2064,7 +2064,7 @@ const machine_t machines[] = {
             .max_multi = 0
         },
         .bus_flags = MACHINE_PC,
-        .flags = MACHINE_VIDEO | MACHINE_MOUSE | MACHINE_MFM,
+        .flags = MACHINE_VIDEO | MACHINE_MOUSE,
         .ram = {
             .min = 128,
             .max = 640,
@@ -2101,7 +2101,7 @@ const machine_t machines[] = {
             .max_multi = 0
         },
         .bus_flags = MACHINE_PC,
-        .flags = MACHINE_FLAGS_NONE,
+        .flags = MACHINE_MFM,
         .ram = {
             .min = 128,
             .max = 640,
