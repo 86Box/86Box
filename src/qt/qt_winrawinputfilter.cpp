3b68bef4ebdeab11340cbc63b76766bc03338060
Cacodemon345 <wahil1976@outlook.com>
2023-02-24 15:05:32 +0600

    qt_winrawinputfilter: Poll for 4th and 5th mouse buttons

--- src/qt/qt_winrawinputfilter.cpp
+++ src/qt/qt_winrawinputfilter.cpp
@@ -340,6 +340,16 @@ WindowsRawInputFilter::mouse_handle(PRAWINPUT raw)
     else if (state.usButtonFlags & RI_MOUSE_RIGHT_BUTTON_UP)
         buttons &= ~2;
 
+    if (state.usButtonFlags & RI_MOUSE_BUTTON_4_DOWN)
+        mousestate.buttons |= 8;
+    else if (state.usButtonFlags & RI_MOUSE_BUTTON_4_UP)
+        mousestate.buttons &= ~8;
+
+    if (state.usButtonFlags & RI_MOUSE_BUTTON_5_DOWN)
+        mousestate.buttons |= 16;
+    else if (state.usButtonFlags & RI_MOUSE_BUTTON_5_UP)
+        mousestate.buttons &= ~16;
+    
     if (state.usButtonFlags & RI_MOUSE_WHEEL) {
         dwheel += (SHORT) state.usButtonData / 120;
     }
