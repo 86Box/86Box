df61dc1b22e78cf5d5edd74e129a74a12e0c1ef3
OBattler <oubattler@gmail.com>
2023-03-27 20:42:57 +0200

    Test commit with dummy changes.

--- src/86box.c
+++ src/86box.c
@@ -8,8 +8,6 @@
  *
  *          Main emulator module where most things are controlled.
  *
- *
- *
  * Authors: Sarah Walker, <https://pcem-emulator.co.uk/>
  *          Miran Grca, <mgrca8@gmail.com>
  *          Fred N. van Kempen, <decwiz@yahoo.com>
