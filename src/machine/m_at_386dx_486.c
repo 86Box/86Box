cb9b72c31b7cd2ed282611727b1ab181f7b2c936
OBattler <oubattler@gmail.com>
2023-06-14 22:40:09 +0200

    Corrected the Flash and keyboard controller of the Shuttle HOT-433A.

--- src/machine/m_at_386dx_486.c
+++ src/machine/m_at_386dx_486.c
@@ -1593,10 +1593,8 @@ machine_at_hot433_init(const machine_t *model)
     device_add(&umc_hb4_device);
     device_add(&umc_8886af_device);
     device_add(&um8669f_device);
-    // device_add(&intel_flash_bxt_device);
-    device_add(&sst_flash_29ee010_device);
-    // device_add(&keyboard_at_ami_device);
-    device_add(&keyboard_ps2_ami_device);
+    device_add(&winbond_flash_w29c010_device);
+    device_add(&keyboard_at_ami_device);
 
     return ret;
 }
