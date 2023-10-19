378ef1ae6d6bfda5f7ab91f84447704f3ae8ea4e
OBattler <oubattler@gmail.com>
2023-10-20 00:20:57 +0200

    The Tandy 1000 HX no longer incorrectly support 256 kB of RAM, fixes #3767.

--- src/machine/machine_table.c
+++ src/machine/machine_table.c
@@ -1472,7 +1472,7 @@ const machine_t machines[] = {
         .bus_flags = MACHINE_PC,
         .flags = MACHINE_VIDEO_FIXED,
         .ram = {
-            .min = 256,
+            .min = 384,
             .max = 640,
             .step = 128
         },
