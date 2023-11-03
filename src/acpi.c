4b9b664571358cde4ac713e03cc91fa90a10ecf2
OBattler <oubattler@gmail.com>
2023-11-03 05:52:26 +0100

    ACPI now correctly only does the power on resume event on power on, not software-initiated hard resets, fixes #3794.

--- src/acpi.c
+++ src/acpi.c
@@ -1653,9 +1653,6 @@ acpi_reset(void *priv)
             dev->regs.gpi_val |= 0x00000004;
     }
 
-    /* Power on always generates a resume event. */
-    dev->regs.pmsts |= 0x8100;
-
     acpi_rtc_status = 0;
 
     acpi_update_irq(dev);
@@ -1761,6 +1758,9 @@ acpi_init(const device_t *info)
     timer_on_auto(&dev->pwrbtn_timer, 16. * 1000.);
 
     acpi_reset(dev);
+
+    /* Power on always generates a resume event. */
+    dev->regs.pmsts |= 0x8100;
 
     acpi_enabled = 1;
     return dev;
