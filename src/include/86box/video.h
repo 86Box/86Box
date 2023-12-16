e4696aa2e96f9a05fb87bd79646c35a914c74bbb
TC1995 <mariogplayer90@gmail.com>
2023-12-16 20:17:55 +0100

    Undev branch the Matrox Millennium since it is now mostly usable.

--- src/include/86box/video.h
+++ src/include/86box/video.h
@@ -431,9 +431,9 @@ extern const device_t ht216_32_standalone_device;
 extern const device_t im1024_device;
 extern const device_t pgc_device;
 
-#    if defined(DEV_BRANCH) && defined(USE_MGA)
 /* Matrox MGA */
 extern const device_t millennium_device;
+#    if defined(DEV_BRANCH)
 extern const device_t mystique_device;
 extern const device_t mystique_220_device;
 #    endif
