4f392ca8e3592674eacbdc2db616b1f5e512c85b
OBattler <oubattler@gmail.com>
2024-01-07 01:42:34 +0100

    SM(S)C FDC37C669 Super I/O chip rewrite and proper FDC power down behavior, fixes floppy drive errors on the new Daeweoo machine.

--- src/machine/m_at_socket7.c
+++ src/machine/m_at_socket7.c
@@ -1127,7 +1127,7 @@ machine_at_ms5146_init(const machine_t *model)
     pci_register_slot(0x10, PCI_CARD_NORMAL,      4, 1, 2, 3);
 
     device_add(&sis_5571_device);
-    device_add(&keyboard_ps2_ali_pci_device);
+    device_add(&keyboard_ps2_ami_pci_device);
     device_add(&w83877f_device);
     device_add(&sst_flash_29ee010_device);
 
@@ -1151,13 +1151,13 @@ machine_at_cb52x_si_init(const machine_t *model)
     pci_register_slot(0x00, PCI_CARD_NORTHBRIDGE, 0, 0, 0, 0);
     pci_register_slot(0x01, PCI_CARD_SOUTHBRIDGE, 1, 2, 3, 4);
     pci_register_slot(0x0D, PCI_CARD_NORMAL,      1, 2, 3, 4);
-    pci_register_slot(0x0E, PCI_CARD_NORMAL,      2, 3, 4, 1);
+    pci_register_slot(0x0B, PCI_CARD_NORMAL,      2, 3, 4, 1);
     pci_register_slot(0x0F, PCI_CARD_NORMAL,      3, 4, 1, 2);
-    pci_register_slot(0x10, PCI_CARD_NORMAL,      4, 1, 2, 3);
+    pci_register_slot(0x07, PCI_CARD_NORMAL,      4, 1, 2, 3);
 
     device_add(&sis_5571_device);
-    device_add(&keyboard_ps2_ali_pci_device);
-    device_add(&fdc37c669_device);
+    device_add(&keyboard_ps2_ami_pci_device);
+    device_add(&fdc37c669_370_device);
     device_add(&sst_flash_29ee010_device);
 
     return ret;
