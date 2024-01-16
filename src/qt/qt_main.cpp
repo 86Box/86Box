b6582383337e926c719c96a9ee36b9100d42d074
Cacodemon345 <wahil1976@outlook.com>
2024-01-16 17:11:33 +0600

    Force all widgets to be embedded windows on Windows

--- src/qt/qt_main.cpp
+++ src/qt/qt_main.cpp
@@ -176,6 +176,7 @@ main(int argc, char *argv[])
 
 #ifdef Q_OS_WINDOWS
     Q_INIT_RESOURCE(darkstyle);
+    QApplication::SetAttribute(Qt::AA_NativeWindows);
 
     if (!windows_is_light_theme()) {
         QFile f(":qdarkstyle/dark/darkstyle.qss");
