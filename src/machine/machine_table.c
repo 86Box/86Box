afe0627177bb9c6e52629d7a0d22f3465a13a582
OBattler <oubattler@gmail.com>
2024-01-28 05:06:42 +0100

    J-Bond PCI400C-B: Correct the bus flag - the machine in fact supports VLB but not PS/2 mice.

--- src/machine/machine_table.c
+++ src/machine/machine_table.c
@@ -7482,7 +7482,7 @@ const machine_t machines[] = {
             .min_multi = 0,
             .max_multi = 0
         },
-        .bus_flags = MACHINE_PS2_PCI,
+        .bus_flags = MACHINE_PCIV,
         .flags = MACHINE_IDE_DUAL | MACHINE_APM,
         .ram = {
             .min = 2048,
