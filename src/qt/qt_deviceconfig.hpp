036790e8a5a0658607e6fc55cd94b8adaf0c4068
Cacodemon345 <wahil1976@outlook.com>
2024-02-02 17:00:38 +0600

    DeviceConfig change (part 1)

--- src/qt/qt_deviceconfig.hpp
+++ src/qt/qt_deviceconfig.hpp
@@ -2,6 +2,7 @@
 #define QT_DEVICECONFIG_HPP
 
 #include <QDialog>
+#include <QWidget>
 
 #include "qt_settings.hpp"
 
@@ -22,7 +23,7 @@ public:
     explicit DeviceConfig(QWidget *parent = nullptr);
     ~DeviceConfig();
 
-    static void    ConfigureDevice(const _device_ *device, int instance = 0, Settings *settings = nullptr, bool atRuntime = false);
+    static void    ConfigureDevice(const _device_ *device, int instance = 0, QWidget *settings = nullptr, bool atRuntime = false);
     static QString DeviceName(const _device_ *device, const char *internalName, int bus);
 
 private:
