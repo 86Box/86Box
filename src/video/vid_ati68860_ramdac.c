f8647f07a318adbd15fd0d9dfa5d96a9c8133ea4
TC1995 <mariogplayer90@gmail.com>
2024-02-07 03:06:19 +0100

    Mach64 temporary updates:
    Temporarily replace the ATI68860 8bpp renderer with a clone one while the current renderer (8bpp) is being fixed for proper colors on the Mach64.

--- src/video/vid_ati68860_ramdac.c
+++ src/video/vid_ati68860_ramdac.c
@@ -130,7 +130,8 @@ ati68860_ramdac_out(uint16_t addr, uint8_t val, void *priv, svga_t *svga)
                             ramdac->render = svga_render_4bpp_highres;
                             break;
                         case 0x83:
-                            ramdac->render = svga_render_8bpp_highres;
+                            /*FIXME*/
+                            ramdac->render = svga_render_8bpp_clone_highres;
                             break;
                         case 0xa0:
                         case 0xb0:
@@ -155,7 +156,8 @@ ati68860_ramdac_out(uint16_t addr, uint8_t val, void *priv, svga_t *svga)
                             ramdac->render = svga_render_RGBA8888_highres;
                             break;
                         default:
-                            ramdac->render = svga_render_8bpp_highres;
+                            /*FIXME*/
+                            ramdac->render = svga_render_8bpp_clone_highres;
                             break;
                     }
                     break;
@@ -235,7 +237,8 @@ ati68860_ramdac_init(UNUSED(const device_t *info))
     ati68860_ramdac_t *ramdac = (ati68860_ramdac_t *) malloc(sizeof(ati68860_ramdac_t));
     memset(ramdac, 0, sizeof(ati68860_ramdac_t));
 
-    ramdac->render = svga_render_8bpp_highres;
+    /*FIXME*/
+    ramdac->render = svga_render_8bpp_clone_highres;
 
     return ramdac;
 }
