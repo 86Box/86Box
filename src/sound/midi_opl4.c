758cad6d1765c6c1988d5f1f5c4b96efdb26ecd3
Cacodemon345 <wahil1976@outlook.com>
2024-02-14 00:46:55 +0600

    Revert "And in sound/midi_opl4.c."
    This reverts commit fda5cee1f35fae18bc9a0df33876c34e85eae09e.

--- src/sound/midi_opl4.c
+++ src/sound/midi_opl4.c
@@ -437,6 +437,7 @@ note_on(uint8_t note, uint8_t velocity, MIDI_CHANNEL_DATA *midi_channel, opl4_mi
     const YRW801_WAVE_DATA       *wave_data[2];
     VOICE_DATA                   *voice[2];
     uint8_t                       i = 0, voices = 0;
+    uint32_t                      j = 0;
 
     while (opl4_midi->gen_in_progress) { }
 
