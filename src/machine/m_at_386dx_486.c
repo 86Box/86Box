edee5fd83950e352ac569fcd990a14c74f289407
cartifanwlr <ucucanok@outlook.com.tr>
2024-02-16 20:45:37 +0300

    More 486 machine changes that I missed

--- src/machine/m_at_386dx_486.c
+++ src/machine/m_at_386dx_486.c
@@ -1961,18 +1961,21 @@ machine_at_actionpc2600_init(const machine_t *model)
     machine_at_common_init(model);
 
     pci_init(PCI_CONFIG_TYPE_1);
-    pci_register_slot(0x10, PCI_CARD_NORTHBRIDGE, 0, 0, 0, 0);
+    pci_register_slot(0x10, PCI_CARD_NORTHBRIDGE, 0, 0, 0, 3);
     pci_register_slot(0x12, PCI_CARD_SOUTHBRIDGE, 1, 2, 3, 4);
     pci_register_slot(0x0C, PCI_CARD_NORMAL,      1, 2, 3, 4);
     pci_register_slot(0x0D, PCI_CARD_NORMAL,      4, 1, 2, 3);
-    pci_register_slot(0x0E, PCI_CARD_NORMAL,      3, 4, 1, 2);
+    pci_register_slot(0x0E, PCI_CARD_VIDEO,       0, 0, 0, 0);
     pci_register_slot(0x0F, PCI_CARD_NORMAL,      2, 3, 4, 1);
 
     device_add(&umc_hb4_device);
     device_add(&umc_8886af_device);
-    device_add(&um8669f_device);
+    device_add(&fdc37c665_device);
     device_add(&intel_flash_bxt_device);
     device_add(&keyboard_ps2_tg_ami_device);
+
+    if (gfxcard[0] == VID_INTERNAL)
+        device_add(&tgui9440_onboard_pci_device);
 
     return ret;
 }
@@ -1992,14 +1995,15 @@ machine_at_actiontower8400_init(const machine_t *model)
     pci_init(PCI_CONFIG_TYPE_1);
     pci_register_slot(0x10, PCI_CARD_NORTHBRIDGE, 0, 0, 0, 0);
     pci_register_slot(0x12, PCI_CARD_SOUTHBRIDGE, 1, 2, 3, 4);
-    if (gfxcard[0] == VID_INTERNAL)
-        pci_register_slot(0x15, PCI_CARD_VIDEO,   0, 0, 0, 0);
+    pci_register_slot(0x15, PCI_CARD_VIDEO,       0, 0, 0, 0);
+    pci_register_slot(0x16, PCI_CARD_IDE,         0, 0, 0, 0);
     pci_register_slot(0x13, PCI_CARD_NORMAL,      1, 2, 3, 4);
     pci_register_slot(0x14, PCI_CARD_NORMAL,      2, 3, 4, 1);
 
     device_add(&umc_hb4_device);
     device_add(&umc_8886af_device);
     device_add(&fdc37c665_device);
+    device_add(&ide_cmd640_pci_device);
     device_add(&intel_flash_bxt_device); // The ActionPC 2600 has this so I'm gonna assume this does too.
     device_add(&keyboard_ps2_ami_pci_device);
     if (gfxcard[0] == VID_INTERNAL)
