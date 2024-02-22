2b6aac37708670ce75c71ae1559e7cb5d26409f6
Cacodemon345 <wahil1976@outlook.com>
2024-02-22 23:10:55 +0600

    Avoid clobbering POST code logging

--- src/video/vid_svga.c
+++ src/video/vid_svga.c
@@ -577,6 +577,7 @@ svga_recalctimings(svga_t *svga)
     double           _dispontime8514 = 0.0;
     double           _dispofftime8514 = 0.0;
     double           disptime8514 = 0.0;
+    int              card_dpms = 0;
 #ifdef ENABLE_SVGA_LOG
     int              vsyncend;
     int              vblankend;
@@ -946,6 +947,7 @@ svga_recalctimings(svga_t *svga)
     if (!svga->force_old_addr)
         svga_recalc_remap_func(svga);
 
+    card_dpms = svga->dpms;
     if (!svga->override && svga->render == svga_render_blank && !(svga->crtc[0x17] & 0x80))
         svga->dpms = 1;
 
@@ -962,7 +964,8 @@ svga_recalctimings(svga_t *svga)
             video_blit_memtoscreen_monitor(x_start, y_start, svga->monitor->mon_xsize + x_add, svga->monitor->mon_ysize + y_add, svga->monitor_index);
             video_wait_for_buffer_monitor(svga->monitor_index);
             svga->dpms_ui = 1;
-            ui_sb_set_text_w(plat_get_string(IDS_2143));
+            if (!postcard_enabled || (postcard_enabled && card_dpms))
+                ui_sb_set_text_w(plat_get_string(IDS_2143));
         }
     } else if (svga->dpms_ui) {
         svga->dpms_ui = 0;
