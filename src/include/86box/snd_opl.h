50ac0d1e0341606cc898e4684f39d5261d0dfa9b
Cacodemon345 <wahil1976@outlook.com>
2024-03-01 01:58:48 +0600

    Add FM_ESFM type

--- src/include/86box/snd_opl.h
+++ src/include/86box/snd_opl.h
@@ -22,7 +22,8 @@ enum fm_type {
     FM_YMF262  = 1, /* OPL3 */
     FM_YMF289B = 2, /* OPL3-L */
     FM_YMF278B = 3, /* OPL 4 */
-    FM_MAX     = 4
+    FM_ESFM    = 4, /* ESFM */
+    FM_MAX     = 5
 };
 
 enum fm_driver {
@@ -45,6 +46,7 @@ extern uint8_t fm_driver_get(int chip_id, fm_drv_t *drv);
 
 extern const fm_drv_t nuked_opl_drv;
 extern const fm_drv_t ymfm_drv;
+extern const fm_drv_t esfmu_opl_drv;
 
 #ifdef EMU_DEVICE_H
 extern const device_t ym3812_nuked_device;
@@ -54,6 +56,8 @@ extern const device_t ym3812_ymfm_device;
 extern const device_t ymf262_ymfm_device;
 extern const device_t ymf289b_ymfm_device;
 extern const device_t ymf278b_ymfm_device;
+
+extern const device_t esfm_esfmu_device;
 #endif
 
 #endif /*SOUND_OPL_H*/
