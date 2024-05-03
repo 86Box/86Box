93ffbdee51c8441e801d90430fb7b4b200f6bf06
OBattler <oubattler@gmail.com>
2024-05-03 19:06:31 +0200

    Reverted the LPT DAC and DSS changes.

--- src/sound/snd_lpt_dac.c
+++ src/sound/snd_lpt_dac.c
@@ -5,14 +5,12 @@
 #include <wchar.h>
 
 #include "cpu.h"
-
-#include <specstrings.h>
 #include <86box/86box.h>
 #include <86box/filters.h>
-#include <86box/timer.h>
 #include <86box/lpt.h>
 #include <86box/machine.h>
 #include <86box/sound.h>
+#include <86box/timer.h>
 #include <86box/plat_unused.h>
 
 typedef struct lpt_dac_t {
@@ -50,14 +48,6 @@ dac_write_data(uint8_t val, void *priv)
     } else
         lpt_dac->dac_val_l = lpt_dac->dac_val_r = val;
     dac_update(lpt_dac);
-}
-
-static void
-dac_strobe(uint8_t old, uint8_t val, void *priv)
-{
-    lpt_dac_t *lpt_dac = (lpt_dac_t *) priv;
-
-    lpt_dac->channel = val;
 }
 
 static void
@@ -120,31 +110,25 @@ dac_close(void *priv)
 }
 
 const lpt_device_t lpt_dac_device = {
-    .name             = "LPT DAC / Covox Speech Thing",
-    .internal_name    = "lpt_dac",
-    .init             = dac_init,
-    .close            = dac_close,
-    .write_data       = dac_write_data,
-    .write_ctrl       = dac_write_ctrl,
-    .autofeed         = NULL,
-    .strobe           = dac_strobe,
-    .read_status      = dac_read_status,
-    .read_ctrl        = NULL,
-    .epp_write_data   = NULL,
-    .epp_request_read = NULL
+    .name          = "LPT DAC / Covox Speech Thing",
+    .internal_name = "lpt_dac",
+    .init          = dac_init,
+    .close         = dac_close,
+    .write_data    = dac_write_data,
+    .write_ctrl    = dac_write_ctrl,
+    .read_data     = NULL,
+    .read_status   = dac_read_status,
+    .read_ctrl     = NULL
 };
 
 const lpt_device_t lpt_dac_stereo_device = {
-    .name             = "Stereo LPT DAC",
-    .internal_name    = "lpt_dac_stereo",
-    .init             = dac_stereo_init,
-    .close            = dac_close,
-    .write_data       = dac_write_data,
-    .write_ctrl       = dac_write_ctrl,
-    .autofeed         = NULL,
-    .strobe           = dac_strobe,
-    .read_status      = dac_read_status,
-    .read_ctrl        = NULL,
-    .epp_write_data   = NULL,
-    .epp_request_read = NULL
+    .name          = "Stereo LPT DAC",
+    .internal_name = "lpt_dac_stereo",
+    .init          = dac_stereo_init,
+    .close         = dac_close,
+    .write_data    = dac_write_data,
+    .write_ctrl    = dac_write_ctrl,
+    .read_data     = NULL,
+    .read_status   = dac_read_status,
+    .read_ctrl     = NULL
 };
