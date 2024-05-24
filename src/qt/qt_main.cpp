b97cbceb0743881208ca222c2a79ea12ee395c4e
usergithub64 <58270614+usergithub64@users.noreply.github.com>
2024-05-24 11:50:01 +0300

    Fixes for translation not working in the main window
    Also need to add the appropriate translation strings to the language files

--- src/qt/qt_main.cpp
+++ src/qt/qt_main.cpp
@@ -223,8 +223,8 @@ main(int argc, char *argv[])
     if(!util::compareUuid()) {
         QMessageBox movewarnbox;
         movewarnbox.setIcon(QMessageBox::Icon::Warning);
-        movewarnbox.setText("This machine might have been moved or copied.");
-        movewarnbox.setInformativeText("In order to ensure proper networking functionality, 86Box needs to know if this machine was moved or copied.\n\nSelect \"I Copied It\" if you are not sure.");
+        movewarnbox.setText(QObject::tr("This machine might have been moved or copied."));
+        movewarnbox.setInformativeText(QObject::tr("In order to ensure proper networking functionality, 86Box needs to know if this machine was moved or copied.\n\nSelect \"I Copied It\" if you are not sure."));
         const QPushButton *movedButton  = movewarnbox.addButton(QObject::tr("I Moved It"), QMessageBox::AcceptRole);
         const QPushButton *copiedButton = movewarnbox.addButton(QObject::tr("I Copied It"), QMessageBox::DestructiveRole);
         QPushButton       *cancelButton = movewarnbox.addButton(QObject::tr("Cancel"), QMessageBox::RejectRole);
