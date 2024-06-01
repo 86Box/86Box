066f153dee624310f29f5df67f2b99ac72e1a07f
Jose Phillips <jose@latinol.com>
2024-06-01 18:39:40 -0500

    Added Machine Motherboard Acer100T

--- src/include/86box/chipset.h
+++ src/include/86box/chipset.h
@@ -24,6 +24,7 @@ extern const device_t acc2168_device;
 extern const device_t ali1217_device;
 extern const device_t ali1429_device;
 extern const device_t ali1429g_device;
+extern const device_t ali1409_device;
 extern const device_t ali1435_device;
 extern const device_t ali1489_device;
 extern const device_t ali1531_device;
