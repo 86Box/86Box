d8b02330e615f250103cf96d84f16cc97cd72c82
MaxwellS04 <tongpauz@gmail.com>
2024-08-28 12:28:02 +0700

    Forgotten one more change

--- src/machine/machine_table.c
+++ src/machine/machine_table.c
@@ -5981,7 +5981,7 @@ const machine_t machines[] = {
         .flags = MACHINE_IDE | MACHINE_VIDEO | MACHINE_APM,
         .ram = {
             .min = 1024,
-            .max = 32768,
+            .max = 65536,
             .step = 1024
         },
         .nvrmask = 127,
