9c1d9e2d82ca93dec5487bf85eaa615ad36eb161
OBattler <oubattler@gmail.com>
2024-09-24 04:37:26 +0200

    MO and ZIP.

--- src/disk/zip.c
+++ src/disk/zip.c
@@ -1134,6 +1134,8 @@ zip_blocks(zip_t *dev, int32_t *len, UNUSED(int first_batch), int out)
         if (out) {
             if (fwrite(dev->buffer + (i << 9), 1, 512, dev->drv->fp) != 512)
                 fatal("zip_blocks(): Error writing data\n");
+
+            fflush(dev->drv->fp);
         } else {
             if (fread(dev->buffer + (i << 9), 1, 512, dev->drv->fp) != 512)
                 fatal("zip_blocks(): Error reading data\n");
@@ -2125,6 +2127,8 @@ zip_phase_data_out(scsi_common_t *sc)
                 if (fwrite(dev->buffer, 1, 512, dev->drv->fp) != 512)
                     fatal("zip_phase_data_out(): Error writing data\n");
             }
+
+            fflush(dev->drv->fp);
             break;
         case GPCMD_MODE_SELECT_6:
         case GPCMD_MODE_SELECT_10:
