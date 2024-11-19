463badd61369f1ec280be6b3e0661cda912ca6cc
OBattler <oubattler@gmail.com>
2024-11-19 04:08:21 +0100

    kB -> KB.

--- src/video/vid_nga.c
+++ src/video/vid_nga.c
@@ -643,11 +643,11 @@ const device_config_t nga_config[] = {
         .default_int = 64,
         .selection = {
             {
-                .description = "32 kB",
+                .description = "32 KB",
                 .value = 32
             },
             {
-                .description = "64 kB",
+                .description = "64 KB",
                 .value = 64
             },
             {
