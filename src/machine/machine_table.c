738daa9a4eaa5bb7515a490e78bedbc1d058ac27
OBattler <oubattler@gmail.com>
2024-11-26 11:38:03 +0100

    Fixed a compile-breaking typo in machine/machine_table.c, fixes #5000.

--- src/machine/machine_table.c
+++ src/machine/machine_table.c
@@ -4113,7 +4113,7 @@ const machine_t machines[] = {
             .max_multi = 0
         },
         .bus_flags = MACHINE_AT,
-        .flags = MACHINE_FLGAS_NONE,
+        .flags = MACHINE_FLAGS_NONE,
         .ram = {
             .min = 1024,
             .max = 16384,
