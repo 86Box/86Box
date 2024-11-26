781320817f7ada8e532573c677bb885545966398
OBattler <oubattler@gmail.com>
2024-11-26 12:01:27 +0100

    Aaeon SBC-350: Restore the IDE flag and actually fix IDE and Super I/O chip initialization.

--- src/machine/machine_table.c
+++ src/machine/machine_table.c
@@ -4113,7 +4113,7 @@ const machine_t machines[] = {
             .max_multi = 0
         },
         .bus_flags = MACHINE_AT,
-        .flags = MACHINE_FLAGS_NONE,
+        .flags = MACHINE_IDE,
         .ram = {
             .min = 1024,
             .max = 16384,
