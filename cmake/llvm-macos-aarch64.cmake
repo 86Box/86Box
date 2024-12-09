8239b2c58719425a979f2e110e42318b5861bc2e
Jose Phillips <jose@latinol.com>
2024-12-08 20:40:00 -0500

    Update llvm-macos-aarch64.cmake

--- cmake/llvm-macos-aarch64.cmake
+++ cmake/llvm-macos-aarch64.cmake
@@ -18,6 +18,5 @@
 
 string(APPEND CMAKE_C_FLAGS_INIT    " -march=armv8.5-a+simd")
 string(APPEND CMAKE_CXX_FLAGS_INIT  " -march=armv8.5-a+simd")
-string(APPEND )
 
 include(${CMAKE_CURRENT_LIST_DIR}/flags-gcc.cmake)
