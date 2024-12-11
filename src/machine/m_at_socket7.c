7250da349e67efccdf7c0e3960a50e1486b54788
AstroMadson <98224233+AstroMadson@users.noreply.github.com>
2024-12-11 06:52:21 -0500

    Added the Hitman BIOS

--- src/machine/m_at_socket7.c
+++ src/machine/m_at_socket7.c
@@ -825,6 +825,39 @@ machine_at_i430vx_init(const machine_t *model)
 }
 
 int
+machine_at_hitman_init(const machine_t *model)
+{
+    int ret;
+
+    ret = bios_load_linear_combined2("roms/machines/hitman/1008CY1T.BIO",
+                                     "roms/machines/hitman/1008CY1T.BI1",
+                                     "roms/machines/hitman/1008CY1T.BI2",
+                                     "roms/machines/hitman/1008CY1T.BI3",
+                                     "roms/machines/hitman/1008CY1T.RCV",
+                                     0x3a000, 128);
+
+    if (bios_only || !ret)
+        return ret;
+
+    machine_at_common_init_ex(model, 2);
+
+    pci_init(PCI_CONFIG_TYPE_1);
+    pci_register_slot(0x00, PCI_CARD_NORTHBRIDGE, 0, 0, 0, 0);
+    pci_register_slot(0x08, PCI_CARD_VIDEO,       4, 0, 0, 0);
+    pci_register_slot(0x0D, PCI_CARD_NORMAL,      1, 2, 3, 4);
+    pci_register_slot(0x0E, PCI_CARD_NORMAL,      2, 3, 4, 1);
+    pci_register_slot(0x0F, PCI_CARD_NORMAL,      3, 4, 1, 2);
+    pci_register_slot(0x10, PCI_CARD_NORMAL,      4, 1, 2, 3);
+    pci_register_slot(0x07, PCI_CARD_SOUTHBRIDGE, 0, 0, 0, 4);
+    device_add(&i430vx_device);
+    device_add(&piix3_device);
+    device_add(&fdc37c932fr_device);
+    device_add(&intel_flash_bxt_ami_device);
+
+    return ret;
+}
+
+int
 machine_at_ma23c_init(const machine_t *model)
 {
     int ret;
