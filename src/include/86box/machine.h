881e4d6bd6cc7279b70dc0b16c5f10984781ac8f
Enzo <enzomlg88@gmail.com>
2025-02-08 11:51:08 -0800

    Update machine.h
    Added 486Bekp407

--- src/include/86box/machine.h
+++ src/include/86box/machine.h
@@ -572,6 +572,7 @@ extern int machine_at_sb486p_init(const machine_t *);
 extern int machine_at_486sp3_init(const machine_t *);
 extern int machine_at_486sp3c_init(const machine_t *);
 extern int machine_at_486sp3g_init(const machine_t *);
+extern int machine_at_486bekp407_init(const machine_t *);
 extern int machine_at_486ap4_init(const machine_t *);
 extern int machine_at_g486vpa_init(const machine_t *);
 extern int machine_at_486vipio2_init(const machine_t *);
