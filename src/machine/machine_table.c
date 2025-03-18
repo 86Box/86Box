96644d137f785cec857b9a1c217dae65d563d50b
rushieda <185547947+rushieda@users.noreply.github.com>
2025-03-18 19:57:14 +0300

    Add the Soyo SY-7SBB (SiS 600, Socket 370)

--- src/machine/machine_table.c
+++ src/machine/machine_table.c
@@ -16227,6 +16227,48 @@ const machine_t machines[] = {
         .snd_device = &cmi8738_onboard_device,
         .net_device = NULL
     },
+    /* SiS (5)600 */
+    /* Has the SiS 600 chipset, which is a re-brand of the 5600, with
+       on-chip KBC. */
+    {
+        .name = "[SiS 600] Soyo SY-7SBB",
+        .internal_name = "7sbb",
+        .type = MACHINE_TYPE_SOCKET370,
+        .chipset = MACHINE_CHIPSET_SIS_5600,
+        .init = machine_at_7sbb_init,
+        .p1_handler = NULL,
+        .gpio_handler = NULL,
+        .available_flag = MACHINE_AVAILABLE,
+        .gpio_acpi_handler = NULL,
+        .cpu = {
+            .package = CPU_PKG_SOCKET370,
+            .block = CPU_BLOCK(CPU_CYRIX3S),
+            .min_bus = 60000000,
+            .max_bus = 100000000,
+            .min_voltage = 1800,
+            .max_voltage = 3500,
+            .min_multi = 1.5,
+            .max_multi = 8.0
+        },
+        .bus_flags = MACHINE_PS2_AGP | MACHINE_BUS_USB,
+        .flags = MACHINE_IDE_DUAL | MACHINE_APM | MACHINE_ACPI | MACHINE_USB,
+        .ram = {
+            .min = 8192,
+            .max = 1572864,
+            .step = 1024
+        },
+        .nvrmask = 255,
+        .kbc_device = NULL,
+        .kbc_p1 = 0xff,
+        .gpio = 0xffffffff,
+        .gpio_acpi = 0xffffffff,
+        .device = NULL,
+        .fdc_device = NULL,
+        .sio_device = NULL,
+        .vid_device = NULL,
+        .snd_device = NULL,
+        .net_device = NULL
+    },
 
     /* Miscellaneous/Fake/Hypervisor machines */
     /* Has a Winbond W83977F Super I/O chip with on-chip KBC with AMIKey-2 KBC
