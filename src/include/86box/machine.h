7383b1b296506c6613109747eec9c4931cb9393a
toggo9 <121191375+toggo9@users.noreply.github.com>
2025-06-28 20:47:39 +0200

    Add Acer V12P machine definition.

--- src/include/86box/machine.h
+++ src/include/86box/machine.h
@@ -655,6 +655,7 @@ extern void machine_at_award_common_init(const machine_t *);
 
 extern void machine_at_sp4_common_init(const machine_t *model);
 
+extern int machine_at_v12p_init(const machine_t *);
 extern int machine_at_excaliburpci_init(const machine_t *);
 extern int machine_at_p5mp3_init(const machine_t *);
 extern int machine_at_dellxp60_init(const machine_t *);
