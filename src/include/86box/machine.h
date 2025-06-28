b9e72c0b08624dd51eae9627f18cb971f908cea6
toggo9 <121191375+toggo9@users.noreply.github.com>
2025-06-28 15:15:59 +0200

    Add ICL DRS M35/286 machine definition.

--- src/include/86box/machine.h
+++ src/include/86box/machine.h
@@ -469,6 +469,7 @@ extern int machine_at_quadt386sx_init(const machine_t *);
 extern int machine_at_award286_init(const machine_t *);
 extern int machine_at_gdc212m_init(const machine_t *);
 extern int machine_at_gw286ct_init(const machine_t *);
+extern int machine_at_drsm35286_init(const machine_t *);
 extern int machine_at_senor_scat286_init(const machine_t *);
 extern int machine_at_super286c_init(const machine_t *);
 extern int machine_at_super286tr_init(const machine_t *);
