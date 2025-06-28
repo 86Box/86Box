2230118215f394b98c56473ebb1ac5de38be9da6
toggo9 <121191375+toggo9@users.noreply.github.com>
2025-06-28 20:51:43 +0200

    And also limit the DRS M35/286 to 4 MB RAM.
    More RAM breaks stuff.

--- src/machine/machine_table.c
+++ src/machine/machine_table.c
@@ -4006,7 +4006,7 @@ const machine_t machines[] = {
         .flags = MACHINE_IDE | MACHINE_VIDEO,
         .ram = {
             .min = 512,
-            .max = 5000,
+            .max = 4096,
             .step = 128
         },
         .nvrmask = 127,
