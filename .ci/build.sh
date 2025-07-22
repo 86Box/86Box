c282d91e482dc0e8c5005e1044a569cf4b361a2c
RichardG867 <richardg867@gmail.com>
2025-07-21 21:45:07 -0300

    Jenkins: Fix Linux AppImage build

--- .ci/build.sh
+++ .ci/build.sh
@@ -603,7 +603,7 @@ else
         grep -q " bullseye " /etc/apt/sources.list || echo [!] WARNING: System not running the expected Debian version
 
 	# Establish general dependencies.
-	pkgs="cmake ninja-build pkg-config git wget p7zip-full extra-cmake-modules wayland-protocols tar gzip file appstream qttranslations5-l10n"
+	pkgs="cmake ninja-build pkg-config git wget p7zip-full extra-cmake-modules wayland-protocols tar gzip file appstream qttranslations5-l10n python3-pip"
 	if [ "$(dpkg --print-architecture)" = "$arch_deb" ]
 	then
 		pkgs="$pkgs build-essential"
@@ -1141,55 +1141,32 @@ EOF
 
 		# Copy line.
 		echo "$line" >> AppImageBuilder-generated.yml
-
-		# Workaround for appimage-builder issues 272 and 283 (i686 and armhf are also missing)
-		if [ "$arch_appimage" != "x86_64" -a "$line" = "  files:" ]
-		then
-			# Some mild arbitrary code execution with a dummy package...
-			[ ! -d /runtime ] && sudo apt-get -y -o 'DPkg::Post-Invoke::=mkdir -p /runtime; chmod 777 /runtime' install libsixel1 > /dev/null 2>&1
-
-			echo "    include:" >> AppImageBuilder-generated.yml
-			for loader in "/lib/$libdir/ld-linux"*.so.*
-			do
-				for loader_copy in "$loader" "/lib/$(basename "$loader")"
-				do
-					if [ ! -e "/runtime/compat$loader_copy" ]
-					then
-						mkdir -p "/runtime/compat$(dirname "$loader_copy")"
-						ln -s "$loader" "/runtime/compat$loader_copy"
-					fi
-					echo "    - /runtime/compat$loader_copy" >> AppImageBuilder-generated.yml
-				done
-			done
-		fi
 	done < .ci/AppImageBuilder.yml
 
 	# Download appimage-builder if necessary.
-	appimage_builder_url="https://github.com/AppImageCrafters/appimage-builder/releases/download/v1.1.0/appimage-builder-1.1.0-$(uname -m).AppImage"
-	appimage_builder_binary="$cache_dir/$(basename "$appimage_builder_url")"
-	if [ ! -e "$appimage_builder_binary" ]
+	appimage_builder_commit=22fefa298f9cee922a651a6f65a46fe0ccbfa34e # from issue 376
+	appimage_builder_dir="$cache_dir/appimage-builder-$appimage_builder_commit"
+	if [ ! -d "$appimage_builder_dir" ]
 	then
-		rm -rf "$cache_dir/"*".AppImage" # remove old versions
-		wget -qO "$appimage_builder_binary" "$appimage_builder_url"
+		rm -rf "$cache_dir/appimage-builder-"* # remove old versions
+		python3 -m venv "$appimage_builder_dir" # venv to solve some Debian setuptools headaches
+		"$appimage_builder_dir/bin/pip" install -U "git+https://github.com/AppImageCrafters/appimage-builder.git@$appimage_builder_commit"
 	fi
 
-	# Symlink appimage-builder binary and global cache directory.
+	# Symlink appimage-builder global cache directory.
 	rm -rf appimage-builder.AppImage appimage-builder-cache "$project-"*".AppImage" # also remove any dangling AppImages which may interfere with the renaming process
-	ln -s "$appimage_builder_binary" appimage-builder.AppImage
-	chmod u+x appimage-builder.AppImage
 	mkdir -p "$cache_dir/appimage-builder-cache"
 	ln -s "$cache_dir/appimage-builder-cache" appimage-builder-cache
 
-	# Run appimage-builder in extract-and-run mode for Docker compatibility.
+	# Run appimage-builder from the virtual environment created above.
 	# --appdir is a workaround for appimage-builder issue 270 reported by us.
 	for retry in 1 2 3 4 5
 	do
 		project="$project" project_id="$project_id" project_version="$project_version" project_icon="$project_icon" arch_deb="$arch_deb" \
-			arch_appimage="$arch_appimage" appimage_path="$cwd/$package_name.AppImage" APPIMAGE_EXTRACT_AND_RUN=1 ./appimage-builder.AppImage \
+			arch_appimage="$arch_appimage" appimage_path="$cwd/$package_name.AppImage" "$appimage_builder_dir/bin/appimage-builder" \
 			--recipe AppImageBuilder-generated.yml --appdir "$(grep -oP '^\s+path: \K(.+)' AppImageBuilder-generated.yml)"
 		status=$?
 		[ $status -eq 0 ] && break
-		[ $status -eq 127 ] && rm -rf /tmp/appimage_extracted_*
 	done
 
 	# Remove appimage-builder binary on failure, just in case it's corrupted.
