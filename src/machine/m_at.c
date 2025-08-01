9f3d191bf4e0fe21932af57b6624a4c1f912d810
Umut Çağan Uçanok <ucucanok@outlook.com.tr>
2025-08-01 22:03:34 +0300

    Minor machine changes
    1. Fix chipsets of incorrectly assigned machines (Super-286C, A*Star, Tandy 4000, Vectra VEi8)
    2. Add F82C710 Super I/O for the Samsung SCAT machines because they have them
    3. Add more notes about unemulated or unadded onboard devices
    4. Added friendly names for Intel and TriGem boards that didn't have them

--- src/machine/m_at.c
+++ src/machine/m_at.c
@@ -408,19 +408,3 @@ machine_at_siemens_init(const machine_t *model)
     return ret;
 }
 
-int
-machine_at_wellamerastar_init(const machine_t *model)
-{
-    int ret;
-
-    ret = bios_load_interleaved("roms/machines/wellamerastar/W_3.031_L.BIN",
-                                "roms/machines/wellamerastar/W_3.031_H.BIN",
-                                0x000f0000, 65536, 0);
-
-    if (bios_only || !ret)
-        return ret;
-
-    machine_at_ibm_common_init(model);
-
-    return ret;
-}
