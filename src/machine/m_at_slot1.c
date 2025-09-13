5a35c984e77215873ca4a1d872ba23176f8c74bf
Verloren50000 <110334428+Verloren50000@users.noreply.github.com>
2025-09-13 09:29:25 +0800

    m_at_slot1.c: MSI MS-6119 BIOS Selector Complete Once Again

--- src/machine/m_at_slot1.c
+++ src/machine/m_at_slot1.c
@@ -589,6 +589,8 @@ machine_at_ms6119_init(const machine_t *model)
     device_add_params(&w83977_device, (void *) (W83977TF | W83977_AMI | W83977_NO_NVR));
     device_add(&winbond_flash_w29c020_device);
     spd_register(SPD_TYPE_SDRAM, 0x7, 256);
+    
+    return ret;
 }
 
 int
