36a92d29a1607b586335428d372e954dba181d09
Christopher Lentocha <christopherericlentocha@gmail.com>
2025-09-17 15:39:42 -0400

    Enable `VBE_DISPI_ID_HGSMI`
    Signed-off-by: Christopher Lentocha <christopherericlentocha@gmail.com>

--- src/video/vid_bochs_vbe.c
+++ src/video/vid_bochs_vbe.c
@@ -521,9 +521,7 @@ bochs_vbe_outw(uint16_t addr, uint16_t val, void *priv)
                 case VBE_DISPI_INDEX_ID:
                     if (val == VBE_DISPI_ID0 || val == VBE_DISPI_ID1 || val == VBE_DISPI_ID2 || val == VBE_DISPI_ID3 || val == VBE_DISPI_ID4 || val == VBE_DISPI_ID5 ||
                         /* VBox extensions. */
-                        val == VBE_DISPI_ID_VBOX_VIDEO || val == VBE_DISPI_ID_ANYX ||
-                        // val == VBE_DISPI_ID_HGSMI ||
-                        val == VBE_DISPI_ID_CFG) {
+                        val == VBE_DISPI_ID_VBOX_VIDEO || val == VBE_DISPI_ID_ANYX || val == VBE_DISPI_ID_HGSMI || val == VBE_DISPI_ID_CFG) {
                         dev->vbe_regs[dev->vbe_index] = val;
                     }
                     break;
