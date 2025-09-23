1d9f84b02598fd207e4c6ee77b39789425dbea24
RichardG867 <richardg867@gmail.com>
2025-09-23 12:34:46 -0300

    Revert "Bump GLSL version of default shaders to 1.50"
    This reverts commit 999812e9b986229fc5be853419dfdd178b84058b.

--- src/qt/qt_openglrenderer.cpp
+++ src/qt/qt_openglrenderer.cpp
@@ -75,7 +75,11 @@ extern int video_focus_dim;
 extern int video_refresh_rate;
 
 const char* vertex_shader_default_tex_src =
+#ifdef __APPLE__
         "#version 150\n"
+#else
+        "#version 130\n"
+#endif
         "\n"
         "in vec4 VertexCoord;\n"
         "in vec2 TexCoord;\n"
@@ -89,7 +93,11 @@ const char* vertex_shader_default_tex_src =
         "}\n";
 
 const char* fragment_shader_default_tex_src =
+#ifdef __APPLE__
         "#version 150\n"
+#else
+        "#version 130\n"
+#endif
         "\n"
         "in vec2 texCoord;\n"
         "uniform sampler2D Texture;\n"
@@ -103,7 +111,11 @@ const char* fragment_shader_default_tex_src =
         "}\n";
 
 const char* vertex_shader_default_color_src =
+#ifdef __APPLE__
         "#version 150\n"
+#else
+        "#version 130\n"
+#endif
         "\n"
         "in vec4 VertexCoord;\n"
         "in vec4 Color;\n"
@@ -117,7 +129,11 @@ const char* vertex_shader_default_color_src =
         "}\n";
 
 const char* fragment_shader_default_color_src =
+#ifdef __APPLE__
         "#version 150\n"
+#else
+        "#version 130\n"
+#endif
         "\n"
         "in vec4 color;\n"
         "\n"
