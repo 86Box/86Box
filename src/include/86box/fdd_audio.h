954640bb94f5c8ec56753ff3bce3c222887a60ca
Toni Riikonen <domppari@hotmail.com>
2025-10-01 21:07:23 +0300

    Removed commented code. Returned total_track in configration

--- src/include/86box/fdd_audio.h
+++ src/include/86box/fdd_audio.h
@@ -39,6 +39,7 @@ typedef struct {
     audio_sample_config_t spindlemotor_stop;
     audio_sample_config_t single_track_step;
     audio_sample_config_t multi_track_seek;
+    int total_tracks;
     int samples_per_track;
     double initial_seek_time;
     double initial_seek_time_pcjr;
