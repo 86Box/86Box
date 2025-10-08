5b7ac6fa2116f5e2c5fe1bf79e980bd88e27a035
Cacodemon345 <wahil1976@outlook.com>
2025-10-08 20:10:42 +0600

    Add Intel Advanced/MA (Monaco) (#6297)

--- src/include/86box/video.h
+++ src/include/86box/video.h
@@ -333,6 +333,7 @@ extern const device_t mach64gx_isa_device;
 extern const device_t mach64gx_vlb_device;
 extern const device_t mach64gx_pci_device;
 extern const device_t mach64ct_device;
+extern const device_t mach64ct_device_onboard;
 extern const device_t mach64vt_device;
 extern const device_t mach64vt2_device;
 
