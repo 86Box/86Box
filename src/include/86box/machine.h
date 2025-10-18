ab7de132975706f0790fcb73f67472fd88110158
toggo9 <121191375+toggo9@users.noreply.github.com>
2025-10-18 18:52:49 +0200

    Add two more HP Vectra models. (#6359)
    * Add two 430FX-based Vectras code.
    
    * Add two 430FX-based Vectras machine table entries.
    
    * Organize the 430FX-based Vectras code.
    
    * Add machine definitions for the two 430FX-based Vectras.
    
    * Add the Vectra 500 Series xxx/MT to the GPIO values from the VL/5...
    
    makes it report CPU clocks properly.
    
    * Darn, forgot something.
    
    * Updated and fix the BIOS selector for VE 5/xxx Series 2

--- src/include/86box/machine.h
+++ src/include/86box/machine.h
@@ -970,6 +970,11 @@ extern const device_t  p54tp4xe_device;
 #endif
 extern int             machine_at_p54tp4xe_init(const machine_t *);
 extern int             machine_at_exp8551_init(const machine_t *);
+#ifdef EMU_DEVICE_H
+extern const device_t hpvectravexxx_device;
+#endif
+extern int             machine_at_hpvectravexxx_init(const machine_t *);
+extern int             machine_at_vectra500mt_init(const machine_t *);
 extern int             machine_at_vectra54_init(const machine_t *);
 #ifdef EMU_DEVICE_H
 extern const device_t  thor_device;
