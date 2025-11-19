43ca65bf04d052608a527d4678e85ef215232ad2
RichardG867 <richardg867@gmail.com>
2025-11-19 17:15:45 -0300

    AC97 Codec: Devbranch some non-working stuff

--- src/sound/sound.c
+++ src/sound/sound.c
@@ -187,7 +187,9 @@ static const SOUND_CARD sound_cards[] = {
     /* AC97 */
     { &ad1881_device                },
     { &cs4297a_device               },
+#ifdef USE_SOFTMODEM
     { &si3036_device                },
+#endif
     { NULL                          }
     // clang-format on
 };
