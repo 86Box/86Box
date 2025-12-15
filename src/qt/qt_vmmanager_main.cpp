86Box/86Box

commit 75499ad251b5aac70f3308dc151e32e9bcd1fb92

Manager: Allow starting the currently selected VM by pressing Enter

src/qt/qt_vmmanager_main.cpp
@@ -404,7 +404,7 @@ illegal_chars:
         ui->listView->setCurrentIndex(first_index);
     }
 
-    connect(ui->listView, &QListView::doubleClicked, this, &VMManagerMain::startButtonPressed);
+    connect(ui->listView, &QListView::activated, this, &VMManagerMain::startButtonPressed);
 
     // Load and apply settings
     loadSettings();

commit 61864d5a534a1643b51e8b1fbfa908e245a03f21

VM Manager: prevent single-click activation from starting VM   Fix regression introduced in 75499ad:     activated() triggered VM start on single click.     Use doubleClicked() and Enter shortcut instead.

src/qt/qt_vmmanager_main.cpp
@@ -23,6 +23,7 @@
 #include <QStringListModel>
 #include <QTimer>
 #include <QProgressDialog>
+#include <QShortcut>
 
 #include <thread>
 #include <atomic>
@@ -404,7 +405,12 @@ illegal_chars:
         ui->listView->setCurrentIndex(first_index);
     }
 
-    connect(ui->listView, &QListView::activated, this, &VMManagerMain::startButtonPressed);
+    // Connect double-click to start VM
+    connect(ui->listView, &QListView::doubleClicked, this, &VMManagerMain::startButtonPressed);
+
+    // Connect Enter key to start VM
+    auto enterShortcut = new QShortcut(QKeySequence(Qt::Key_Return), ui->listView);
+    connect(enterShortcut, &QShortcut::activated, this, &VMManagerMain::startButtonPressed);
 
     // Load and apply settings
     loadSettings();