f5fd37bbf128350bf969f3eaa64fac4fe36a80b2
GreaseMonkey <thematrixeatsyou@gmail.com>
2026-01-09 10:01:17 +1300

    Add [i440EX] HP Brio 83xx machine
    I personally own a Brio 8314 and have run tests on it.

--- src/machine/machine_table.c
+++ src/machine/machine_table.c
@@ -18265,6 +18265,55 @@ const machine_t machines[] = {
     },
 
     /* 440EX */
+    /* Has a SM(S)C FDC37C675 Super I/O chip with on-chip KBC with Phoenix
+       MultiKey/42 (version 1.38) KBC firmware. */
+    {
+        .name              = "[i440EX] HP Brio 83xx",
+        .internal_name     = "brio83xx",
+        .type              = MACHINE_TYPE_SLOT1,
+        .chipset           = MACHINE_CHIPSET_INTEL_440EX,
+        .init              = machine_at_brio83xx_init,
+        .p1_handler        = NULL,
+        .gpio_handler      = NULL,
+        .available_flag    = MACHINE_AVAILABLE,
+        .gpio_acpi_handler = NULL,
+        .cpu               = {
+            .package     = CPU_PKG_SLOT1,
+            .block       = CPU_BLOCK_NONE,
+            .min_bus     = 66666667,
+            .max_bus     = 66666667,
+            /* NOTE: Range not confirmed. */
+            .min_voltage = 1800,
+            .max_voltage = 3500,
+            .min_multi   = 1.5,
+            .max_multi   = 8.0
+        },
+        .bus_flags = MACHINE_PS2_AGP | MACHINE_BUS_USB,
+        .flags     = MACHINE_IDE_DUAL | MACHINE_VIDEO | MACHINE_APM | MACHINE_ACPI | MACHINE_USB,
+        .ram       = {
+            /* PC manual says 128 MB max, but 256 MB confirmed to work
+               and 512 MB confirmed to not work. */
+            .min  = 8192,
+            .max  = 262144,
+            .step = 8192
+        },
+        .nvrmask                  = 255,
+        .jumpered_ecp_dma         = 0,
+        .default_jumpered_ecp_dma = -1,
+        .kbc_device               = NULL,
+        .kbc_params               = 0x00000000,
+        .kbc_p1                   = 0x00000cf0,
+        .gpio                     = 0xffffffff,
+        .gpio_acpi                = 0xffffffff,
+        .device                   = NULL,
+        .kbd_device               = NULL,
+        .fdc_device               = NULL,
+        .sio_device               = NULL,
+        .vid_device               = &s3_trio64v2_dx_onboard_pci_device,
+        .snd_device               = NULL,
+        .net_device               = NULL
+    },
+
     /* Has a Winbond W83977TF Super I/O chip with on-chip KBC with AMIKey-2 KBC
        firmware. */
     {
