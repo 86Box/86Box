b3b6943055ed03d96d0b577fe6eaba07ab97540f
OBattler <oubattler@gmail.com>
2026-01-16 16:59:39 +0100

    PIIX4 AMI 1995 NVR: Initialize second I/O port at 72h.

--- src/nvr_at.c
+++ src/nvr_at.c
@@ -1226,7 +1226,8 @@ nvr_at_init(const device_t *info)
             io_sethandler(0x0070, 2,
                           nvr_read, NULL, NULL, nvr_write, NULL, NULL, nvr);
         }
-        if (((info->local & 0x1f) == 0x11) || ((info->local & 0x1f) == 0x17)) {
+        if (((info->local & 0x1f) == 0x11) || ((info->local & 0x1f) == 0x17) ||
+            ((info->local & 0x1f) == 0x18)) {
             io_sethandler(0x0072, 2,
                           nvr_read, NULL, NULL, nvr_write, NULL, NULL, nvr);
         }
