9d1b73196ce3555cfc54f0c785a3f9799122c13d
OBattler <oubattler@gmail.com>
2026-01-22 12:32:23 +0100

    The forgotten qt_defs.hpp.

---
+++ src/qt/qt_defs.hpp
@@ -0,0 +1,10 @@
+#ifndef QT_DEFS_HPP
+#define QT_DEFS_HPP
+
+#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
+#define CHECK_STATE_CHANGED checkStateChanged
+#else
+#define CHECK_STATE_CHANGED stateChanged
+#endif
+
+#endif // QT_DEFS_HPP
