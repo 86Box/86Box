484a61f65a69f964309a3c63741196165c7ac96a
OBattler <oubattler@gmail.com>
2026-01-29 19:47:26 +0100

    Only use checkStateChanged from Qt 6.7 onwards, should fix GitHub Actions builds.

--- src/qt/qt_defs.hpp
+++ src/qt/qt_defs.hpp
@@ -1,7 +1,7 @@
 #ifndef QT_DEFS_HPP
 #define QT_DEFS_HPP
 
-#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
+#if QT_VERSION >= QT_VERSION_CHECK(6, 7, 0)
 #define CHECK_STATE_CHANGED checkStateChanged
 #else
 #define CHECK_STATE_CHANGED stateChanged
