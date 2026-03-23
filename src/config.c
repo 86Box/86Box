dd683522c6b01e729c9625240feb615d96a04a24
Tiago Espinha Gasiba <tiago.gasiba@gmail.com>
2026-03-23 08:36:53 +0100

    unbreak Linux

--- src/config.c
+++ src/config.c
@@ -29,11 +29,12 @@
 
 #ifdef _WIN32
 #    include <ws2tcpip.h>
+#else
+#    include <arpa/inet.h>
 #endif
 #if defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__)
 #    include <sys/types.h>
 #    include <sys/socket.h>
-#    include <arpa/inet.h>
 #endif
 #include <inttypes.h>
 #ifdef ENABLE_CONFIG_LOG
