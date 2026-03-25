d5ee0b83ae01f156862f1c43db6347691341a1a3
RichardG867 <richardg867@gmail.com>
2026-03-24 21:38:14 -0300

    Overhaul device config section migration code
    Eliminated an unnecessary memset from device context setup as all fields are guaranteed to be populated

--- src/device.c
+++ src/device.c
@@ -92,164 +92,68 @@ device_init(void)
 void
 device_set_context(device_context_t *ctx, const device_t *dev, int inst)
 {
-    memset(ctx, 0, sizeof(device_context_t));
+    static const struct {
+        const char *old;
+        const char *new;
+    } section_migrations[] = {
+        { .old = "Standard PS/2 Mouse", .new = "PS/2 Mouse" },
+        { .old = "Microsoft RAMCard for IBM PC", .new = "Microsoft RAMCard" },
+        { .old = "Western Digital WD1007V-SE1 (ESDI)", .new = "WD1007V-SE1 (ESDI)" },
+        { .old = "WD1003 AT MFM/RLL Controller", .new = "WD1003-WAH (MFM/RLL)" },
+        { .old = "ST-11R RLL Fixed Disk Adapter", .new = "ST-11R (RLL)" },
+        { .old = "WD1002A-WX1 MFM Fixed Disk Adapter", .new = "WD1002A-WX1 (MFM)" },
+        { .old = "WD1002A-WX1 MFM Fixed Disk Adapter (No BIOS)", .new = "WD1002A-WX1 (MFM) (No BIOS)" },
+        { .old = "WD1002A-27X RLL Fixed Disk Adapter", .new = "WD1002A-27X (RLL)" },
+        { .old = "WD1004A-WX1 MFM Fixed Disk Adapter", .new = "WD1004A-WX1 (MFM)" },
+        { .old = "WD1004-27X RLL Fixed Disk Adapter", .new = "WD1004-27X (RLL)" },
+        { .old = "WD1004a-27X RLL Fixed Disk Adapter", .new = "WD1004a-27X (RLL)" },
+        { .old = "Victor V86P RLL Fixed Disk Adapter", .new = "Victor V86P Fixed Disk Adapter (RLL)" },
+        { .old = "WDXT-150 XTA Fixed Disk Controller", .new = "WDXT-150 (XTA)" },
+        { .old = "WDXT-150 XTA Fixed Disk Controller (PC3086)", .new = "WDXT-150 (XTA) (PC3086)" },
+        { .old = "ST-50X Fixed Disk Controller", .new = "ST-50X (XTA)" },
+        { .old = "ST-50X Fixed Disk Controller (PC5086)", .new = "ST-50X (XTA) (PC5086)" },
+        { .old = "Acculogic XT IDE", .new = "Acculogic sIDE-1/16 (IDE)" },
+        { .old = "Packard Bell Legend 300SX", .new = "Packard Bell PB300/PB320" },
+        { 0 }
+    };
+
     ctx->dev      = dev;
     ctx->instance = inst;
-    if (inst) {
+    if (inst)
         sprintf(ctx->name, "%s #%i", dev->name, inst);
+    else
+        strncpy(ctx->name, dev->name, sizeof(ctx->name) - 1);
 
-        /* If a numbered section is not present, but a non-numbered of the same name
-           is, rename the non-numbered section to numbered. */
-        const void *sec        = config_find_section(ctx->name);
-        void *      single_sec = config_find_section((char *) dev->name);
-        if ((sec == NULL) && (single_sec != NULL))
-            config_rename_section(single_sec, ctx->name);
-    } else if (!strcmp(dev->name, "PS/2 Mouse")) {
-        sprintf(ctx->name, "%s", dev->name);
+    if (!config_find_section(ctx->name)) {
+        /* Find and migrate old config sections. */
+        void *old_sec;
+        char  old_name[2048] = { 0 };
+        for (int i = 0; section_migrations[i].new; i++) {
+            if (!strcmp(dev->name, section_migrations[i].new)) {
+                if (inst) {
+                    sprintf(old_name, "%s #%i", section_migrations[i].old, inst);
+                    old_sec = config_find_section(old_name);
+                    if (old_sec) {
+                        config_rename_section(old_sec, ctx->name);
+                        return; /* numbering below does not apply */
+                    }
+                }
+                old_sec = config_find_section((char *) section_migrations[i].old);
+                if (old_sec) {
+                    config_rename_section(old_sec, ctx->name);
+                    return; /* if inst, numbering below was already done here */
+                }
+            }
+        }
 
-        /* Migrate the old "Standard PS/2 Mouse" section */
-        const void *sec        = config_find_section(ctx->name);
-        void *      old_sec    = config_find_section("Standard PS/2 Mouse");
-        if ((sec == NULL) && (old_sec != NULL))
-            config_rename_section(old_sec, ctx->name);
-    } else if (!strcmp(dev->name, "Microsoft RAMCard")) {
-        sprintf(ctx->name, "%s", dev->name);
-
-        /* Migrate the old "Microsoft RAMCard for IBM PC" section */
-        const void *sec        = config_find_section(ctx->name);
-        void *      old_sec    = config_find_section("Microsoft RAMCard for IBM PC");
-        if ((sec == NULL) && (old_sec != NULL))
-            config_rename_section(old_sec, ctx->name);
-    } else if (!strcmp(dev->name, "WD1007V-SE1 (ESDI)")) {
-        sprintf(ctx->name, "%s", dev->name);
-
-        /* Migrate the old "Western Digital WD1007V-SE1 (ESDI)" section */
-        const void *sec        = config_find_section(ctx->name);
-        void *      old_sec    = config_find_section("Western Digital WD1007V-SE1 (ESDI)");
-        if ((sec == NULL) && (old_sec != NULL))
-            config_rename_section(old_sec, ctx->name);
-    } else if (!strcmp(dev->name, "WD1003-WAH (MFM/RLL)")) {
-        sprintf(ctx->name, "%s", dev->name);
-
-        /* Migrate the old "WD1003 AT MFM/RLL Controller" section */
-        const void *sec        = config_find_section(ctx->name);
-        void *      old_sec    = config_find_section("WD1003 AT MFM/RLL Controller");
-        if ((sec == NULL) && (old_sec != NULL))
-            config_rename_section(old_sec, ctx->name);
-    } else if (!strcmp(dev->name, "ST-11R (RLL)")) {
-        sprintf(ctx->name, "%s", dev->name);
-
-        /* Migrate the old "ST-11R RLL Fixed Disk Adapter" section */
-        const void *sec        = config_find_section(ctx->name);
-        void *      old_sec    = config_find_section("ST-11R RLL Fixed Disk Adapter");
-        if ((sec == NULL) && (old_sec != NULL))
-            config_rename_section(old_sec, ctx->name);
-    } else if (!strcmp(dev->name, "WD1002A-WX1 (MFM)")) {
-        sprintf(ctx->name, "%s", dev->name);
-
-        /* Migrate the old "WD1002A-WX1 MFM Fixed Disk Adapter" section */
-        const void *sec        = config_find_section(ctx->name);
-        void *      old_sec    = config_find_section("WD1002A-WX1 MFM Fixed Disk Adapter");
-        if ((sec == NULL) && (old_sec != NULL))
-            config_rename_section(old_sec, ctx->name);
-    } else if (!strcmp(dev->name, "WD1002A-WX1 (MFM) (No BIOS)")) {
-        sprintf(ctx->name, "%s", dev->name);
-
-        /* Migrate the old "WD1002A-WX1 MFM Fixed Disk Adapter (No BIOS)" section */
-        const void *sec        = config_find_section(ctx->name);
-        void *      old_sec    = config_find_section("WD1002A-WX1 MFM Fixed Disk Adapter (No BIOS)");
-        if ((sec == NULL) && (old_sec != NULL))
-            config_rename_section(old_sec, ctx->name);
-    } else if (!strcmp(dev->name, "WD1002A-27X (RLL)")) {
-        sprintf(ctx->name, "%s", dev->name);
-
-        /* Migrate the old "WD1002A-27X RLL Fixed Disk Adapter" section */
-        const void *sec        = config_find_section(ctx->name);
-        void *      old_sec    = config_find_section("WD1002A-27X RLL Fixed Disk Adapter");
-        if ((sec == NULL) && (old_sec != NULL))
-            config_rename_section(old_sec, ctx->name);
-    } else if (!strcmp(dev->name, "WD1004A-WX1 (MFM)")) {
-        sprintf(ctx->name, "%s", dev->name);
-
-        /* Migrate the old "WD1004A-WX1 MFM Fixed Disk Adapter" section */
-        const void *sec        = config_find_section(ctx->name);
-        void *      old_sec    = config_find_section("WD1004A-WX1 MFM Fixed Disk Adapter");
-        if ((sec == NULL) && (old_sec != NULL))
-            config_rename_section(old_sec, ctx->name);
-    } else if (!strcmp(dev->name, "WD1004-27X (RLL)")) {
-        sprintf(ctx->name, "%s", dev->name);
-
-        /* Migrate the old "WD1004-27X RLL Fixed Disk Adapter" section */
-        const void *sec        = config_find_section(ctx->name);
-        void *      old_sec    = config_find_section("WD1004-27X RLL Fixed Disk Adapter");
-        if ((sec == NULL) && (old_sec != NULL))
-            config_rename_section(old_sec, ctx->name);
-    } else if (!strcmp(dev->name, "WD1004a-27X (RLL)")) {
-        sprintf(ctx->name, "%s", dev->name);
-
-        /* Migrate the old "WD1004a-27X RLL Fixed Disk Adapter" section */
-        const void *sec        = config_find_section(ctx->name);
-        void *      old_sec    = config_find_section("WD1004a-27X RLL Fixed Disk Adapter");
-        if ((sec == NULL) && (old_sec != NULL))
-            config_rename_section(old_sec, ctx->name);
-    } else if (!strcmp(dev->name, "Victor V86P Fixed Disk Adapter (RLL)")) {
-        sprintf(ctx->name, "%s", dev->name);
-
-        /* Migrate the old "Victor V86P RLL Fixed Disk Adapter" section */
-        const void *sec        = config_find_section(ctx->name);
-        void *      old_sec    = config_find_section("Victor V86P RLL Fixed Disk Adapter");
-        if ((sec == NULL) && (old_sec != NULL))
-            config_rename_section(old_sec, ctx->name);
-    } else if (!strcmp(dev->name, "WDXT-150 (XTA)")) {
-        sprintf(ctx->name, "%s", dev->name);
-
-        /* Migrate the old "WDXT-150 XTA Fixed Disk Controller" section */
-        const void *sec        = config_find_section(ctx->name);
-        void *      old_sec    = config_find_section("WDXT-150 XTA Fixed Disk Controller");
-        if ((sec == NULL) && (old_sec != NULL))
-            config_rename_section(old_sec, ctx->name);
-    } else if (!strcmp(dev->name, "WDXT-150 (XTA) (PC3086)")) {
-        sprintf(ctx->name, "%s", dev->name);
-
-        /* Migrate the old "WDXT-150 XTA Fixed Disk Controller (PC3086)" section */
-        const void *sec        = config_find_section(ctx->name);
-        void *      old_sec    = config_find_section("WDXT-150 XTA Fixed Disk Controller (PC3086)");
-        if ((sec == NULL) && (old_sec != NULL))
-            config_rename_section(old_sec, ctx->name);
-    } else if (!strcmp(dev->name, "ST-50X (XTA)")) {
-        sprintf(ctx->name, "%s", dev->name);
-
-        /* Migrate the old "ST-50X Fixed Disk Controller" section */
-        const void *sec        = config_find_section(ctx->name);
-        void *      old_sec    = config_find_section("ST-50X Fixed Disk Controller");
-        if ((sec == NULL) && (old_sec != NULL))
-            config_rename_section(old_sec, ctx->name);
-    } else if (!strcmp(dev->name, "ST-50X (XTA) (PC5086)")) {
-        sprintf(ctx->name, "%s", dev->name);
-
-        /* Migrate the old "ST-50X Fixed Disk Controller (PC5086)" section */
-        const void *sec        = config_find_section(ctx->name);
-        void *      old_sec    = config_find_section("ST-50X Fixed Disk Controller (PC5086)");
-        if ((sec == NULL) && (old_sec != NULL))
-            config_rename_section(old_sec, ctx->name);
-    } else if (!strcmp(dev->name, "Acculogic sIDE-1/16 (IDE)")) {
-        sprintf(ctx->name, "%s", dev->name);
-
-        /* Migrate the old "Acculogic XT IDE" section */
-        const void *sec        = config_find_section(ctx->name);
-        void *      old_sec    = config_find_section("Acculogic XT IDE");
-        if ((sec == NULL) && (old_sec != NULL))
-            config_rename_section(old_sec, ctx->name);
-    } else if (!strcmp(dev->name, "Packard Bell PB300/PB320")) {
-        sprintf(ctx->name, "%s", dev->name);
-
-        /* Migrate the old "Packard Bell Legend 300SX" section */
-        const void *sec        = config_find_section(ctx->name);
-        void *      old_sec    = config_find_section("Packard Bell Legend 300SX");
-        if ((sec == NULL) && (old_sec != NULL))
-            config_rename_section(old_sec, ctx->name);
-    } else
-        sprintf(ctx->name, "%s", dev->name);
+        if (inst) {
+            /* If a numbered section is not present, but a non-numbered of
+               the same name is, rename the non-numbered section to numbered. */
+            old_sec = config_find_section((char *) dev->name);
+            if (old_sec)
+                config_rename_section(old_sec, ctx->name);
+        }
+    }
 }
 
 static void
