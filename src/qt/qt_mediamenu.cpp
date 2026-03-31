93866384ae98d441dbf421c164371fc43687e195
OBattler <oubattler@gmail.com>
2026-03-31 23:16:19 +0200

    Revert "Resolve conflict."
    This reverts commit 8e6f0e3c85d827552fae23c00847a44d9c9c9107.

--- src/qt/qt_mediamenu.cpp
+++ src/qt/qt_mediamenu.cpp
@@ -178,13 +178,8 @@ MediaMenu::refresh(QMenu *parentMenu)
         // Can make it only appear if the drive is a Yamaha?
         menu->addAction(tr("Add &blank disc"), [this, i]() { cdromInsertBlank(i); })->setCheckable(false);
         menu->addSeparator();
-<<<<<<< Updated upstream
         menu->addAction(getIconWithIndicator(img_icon, pixmap_size, QIcon::Normal, Browse), tr("&Image…"), [this, i]() { cdromMount(i, 0, nullptr); })->setCheckable(false);
         menu->addAction(getIconWithIndicator(folder_icon, pixmap_size, QIcon::Normal, Browse), tr("&Folder…"), [this, i]() { cdromMount(i, 1, nullptr); })->setCheckable(false);
-=======
-        menu->addAction(getIconWithIndicator(QIcon(":/settings/qt/icons/cdrom_image.ico"), pixmap_size, QIcon::Normal, Browse), tr("&Image…"), [this, i]() { cdromMount(i, 0, nullptr); })->setCheckable(false);
-        menu->addAction(getIconWithIndicator(QIcon(":/settings/qt/icons/cdrom_folder.ico"), pixmap_size, QIcon::Normal, Browse), tr("&Folder…"), [this, i]() { cdromMount(i, 1, nullptr); })->setCheckable(false);
->>>>>>> Stashed changes
         menu->addSeparator();
         for (int slot = 0; slot < MAX_PREV_IMAGES; slot++) {
             cdromImageHistoryPos[slot] = menu->children().count();
