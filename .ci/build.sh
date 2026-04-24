5879bc68aa08fbd43ec74995aced59d3a232cbb2
RichardG867 <richardg867@gmail.com>
2026-04-24 00:32:03 -0300

    Jenkins: Add MacPorts openal-soft C++20 workaround

--- .ci/build.sh
+++ .ci/build.sh
@@ -638,6 +638,12 @@ then
 		sudo sed -i -e 's/--enable-libproxy/--disable-libproxy/g' "$wget_portfile"
 		sudo sed -i -e 's/port:libproxy//g' "$wget_portfile"
 
+		# Work around openal-soft failing to build due to C++20. (MacPorts issue 73874)
+		alsoft_portfile="$macports/var/macports/sources/rsync.macports.org/macports/release/tarballs/ports/audio/openal-soft/Portfile"
+		if wc -c "$alsoft_portfile" | grep -q ' 6722 ' then
+			sudo sed -i -e 's/configure.args-append/configure.compiler macports-clang-19\nconfigure.args-append/' "$alsoft_portfile"
+		fi
+
 		while :
 		do
 			# Attempt to install dependencies.
