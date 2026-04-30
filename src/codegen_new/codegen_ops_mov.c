27cbc23d0f1e5b5395ae929bc01a1c3501325016
fuel-pcbox <mszoopers@protonmail.com>
2026-04-30 13:51:29 -0500

    Oops

--- src/codegen_new/codegen_ops_mov.c
+++ src/codegen_new/codegen_ops_mov.c
@@ -967,14 +967,14 @@ ropMOVS_l(codeblock_t *block, ir_data_t *ir, UNUSED(uint8_t opcode), UNUSED(uint
     codegen_check_seg_write(block, ir, &cpu_state.seg_es);
 
     if (op_32 & 0x200) {
-        uop_MEM_LOAD_REG(ir, IREG_temp0_L, seg_base_src, IREG_ESI);
+        uop_MEM_LOAD_REG(ir, IREG_temp0, seg_base_src, IREG_ESI);
         uop_MEM_STORE_REG(ir, seg_base_dst, IREG_EDI, IREG_temp0_L);
         uop_CALL_FUNC(ir, stos_adj_l_a32);
         uop_CALL_FUNC(ir, lods_adj_l_a32);
     } else {
         uop_AND_IMM(ir, IREG_temp1, IREG_ESI, 0xffff);
         uop_AND_IMM(ir, IREG_eaaddr, IREG_EDI, 0xffff);
-        uop_MEM_LOAD_REG(ir, IREG_temp0_L, seg_base_src, IREG_temp1);
+        uop_MEM_LOAD_REG(ir, IREG_temp0, seg_base_src, IREG_temp1);
         uop_MEM_STORE_REG(ir, seg_base_dst, IREG_eaaddr, IREG_temp0_L);
         uop_CALL_FUNC(ir, stos_adj_l_a16);
         uop_CALL_FUNC(ir, lods_adj_l_a16);
