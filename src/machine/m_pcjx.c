52896a13fee000afffa7f82d46ec4b29e340cd35
Jasmine Iwanek <jriwanek@gmail.com>
2026-05-21 23:14:03 -0400

    Update m_pcjx.c

--- src/machine/m_pcjx.c
+++ src/machine/m_pcjx.c
@@ -988,6 +988,30 @@ pcjx_store_mapper_reg1(pcjx_t *pcjx, uint8_t value)
              "[%04X:%08X] mapper[%02X].reg1 raw=%02X stored=%02X\n",
              CS, cpu_state.pc, pcjx->mapper_index, value,
              pcjx->blocks[pcjx->mapper_index].reg1);
+
+    switch (pcjx->mapper_index) {
+        case 0x80:
+        case 0x81:
+        case 0x82:
+        case 0x8c:
+        case 0x8d:
+        case 0x8e:
+        case 0x83:
+        case 0x84:
+        case 0x86:
+        case 0x87:
+        case 0x85:
+        case 0x88:
+        case 0x89:
+        case 0x8a:
+        case 0x90:
+        case 0x91:
+            pcjx_apply_block(pcjx, pcjx->mapper_index);
+            break;
+
+        default:
+            break;
+    }
 }
 
 static void
