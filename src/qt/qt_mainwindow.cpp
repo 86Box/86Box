68901c39eccfc013cd55c37ebbe14e5dff82d5e3
Alexander Babikov <lemondrops358@gmail.com>
2026-06-13 09:02:50 +0500

    Move the auto-pause options to global config

--- src/qt/qt_mainwindow.cpp
+++ src/qt/qt_mainwindow.cpp
@@ -772,12 +772,6 @@ MainWindow::MainWindow(QWidget *parent)
     if (force_43 > 0) {
         ui->actionForce_4_3_display_ratio->setChecked(true);
     }
-    if (do_auto_pause > 0) {
-        ui->actionAuto_pause->setChecked(true);
-    }
-    if (do_auto_diag_pause > 0) {
-        ui->actionAutoDiag_pause->setChecked(true);
-    }
     if (force_constant_mouse > 0) {
         ui->actionUpdate_mouse_every_CPU_frame->setChecked(true);
     }
@@ -1674,7 +1668,7 @@ MainWindow::eventFilter(QObject *receiver, QEvent *event)
         }
     }
 
-    if (receiver == this && do_auto_diag_pause > 0) {
+    if (receiver == this && do_auto_dialog_pause > 0) {
         static auto curdopause = dopause;
         if (event->type() == QEvent::WindowBlocked) {
             window_blocked = true;
@@ -2102,22 +2096,6 @@ MainWindow::on_actionForce_4_3_display_ratio_triggered()
                 renderers[i]->onResize(renderers[i]->width(), renderers[i]->height());
         }
     }
-    config_save();
-}
-
-void
-MainWindow::on_actionAuto_pause_triggered()
-{
-    do_auto_pause ^= 1;
-    ui->actionAuto_pause->setChecked(do_auto_pause > 0 ? true : false);
-    config_save();
-}
-
-void
-MainWindow::on_actionAutoDiag_pause_triggered()
-{
-    do_auto_diag_pause ^= 1;
-    ui->actionAutoDiag_pause->setChecked(do_auto_diag_pause > 0 ? true : false);
     config_save();
 }
 
