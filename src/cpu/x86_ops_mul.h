1b4902a5ee7210d0ded3ad929690c3f6d03f1a53
OBattler <oubattler@gmail.com>
2026-06-25 19:53:02 +0200

    Actually revert the interpreter part of the IMUL stuff as well.

--- src/cpu/x86_ops_mul.h
+++ src/cpu/x86_ops_mul.h
@@ -17,8 +17,12 @@ opIMUL_w_iw_a16(uint32_t fetchdat)
         return 1;
 
     templ = ((int) tempw) * ((int) tempw2);
+    flags_rebuild();
+    if ((templ >> 15) != 0 && (templ >> 15) != -1)
+        cpu_state.flags |= C_FLAG | V_FLAG;
+    else
+        cpu_state.flags &= ~(C_FLAG | V_FLAG);
     cpu_state.regs[cpu_reg].w = templ & 0xffff;
-    setimul16(tempw, tempw2);
 
     CLOCK_CYCLES((cpu_mod == 3) ? 14 : 17);
     PREFETCH_RUN((cpu_mod == 3) ? 14 : 17, 4, rmdat, 1, 0, 0, 0, 0);
@@ -43,8 +47,12 @@ opIMUL_w_iw_a32(uint32_t fetchdat)
         return 1;
 
     templ = ((int) tempw) * ((int) tempw2);
+    flags_rebuild();
+    if ((templ >> 15) != 0 && (templ >> 15) != -1)
+        cpu_state.flags |= C_FLAG | V_FLAG;
+    else
+        cpu_state.flags &= ~(C_FLAG | V_FLAG);
     cpu_state.regs[cpu_reg].w = templ & 0xffff;
-    setimul16(tempw, tempw2);
 
     CLOCK_CYCLES((cpu_mod == 3) ? 14 : 17);
     PREFETCH_RUN((cpu_mod == 3) ? 14 : 17, 4, rmdat, 1, 0, 0, 0, 1);
@@ -70,8 +78,12 @@ opIMUL_l_il_a16(uint32_t fetchdat)
         return 1;
 
     temp64 = ((int64_t) templ) * ((int64_t) templ2);
+    flags_rebuild();
+    if ((temp64 >> 31) != 0 && (temp64 >> 31) != -1)
+        cpu_state.flags |= C_FLAG | V_FLAG;
+    else
+        cpu_state.flags &= ~(C_FLAG | V_FLAG);
     cpu_state.regs[cpu_reg].l = temp64 & 0xffffffff;
-    setimul32(templ, templ2);
 
     CLOCK_CYCLES(25);
     PREFETCH_RUN(25, 6, rmdat, 0, 1, 0, 0, 0);
@@ -96,8 +108,12 @@ opIMUL_l_il_a32(uint32_t fetchdat)
         return 1;
 
     temp64 = ((int64_t) templ) * ((int64_t) templ2);
+    flags_rebuild();
+    if ((temp64 >> 31) != 0 && (temp64 >> 31) != -1)
+        cpu_state.flags |= C_FLAG | V_FLAG;
+    else
+        cpu_state.flags &= ~(C_FLAG | V_FLAG);
     cpu_state.regs[cpu_reg].l = temp64 & 0xffffffff;
-    setimul32(templ, templ2);
 
     CLOCK_CYCLES(25);
     PREFETCH_RUN(25, 6, rmdat, 0, 1, 0, 0, 1);
@@ -125,8 +141,12 @@ opIMUL_w_ib_a16(uint32_t fetchdat)
         tempw2 |= 0xff00;
 
     templ = ((int) tempw) * ((int) tempw2);
+    flags_rebuild();
+    if ((templ >> 15) != 0 && (templ >> 15) != -1)
+        cpu_state.flags |= C_FLAG | V_FLAG;
+    else
+        cpu_state.flags &= ~(C_FLAG | V_FLAG);
     cpu_state.regs[cpu_reg].w = templ & 0xffff;
-    setimul16(tempw, tempw2);
 
     CLOCK_CYCLES((cpu_mod == 3) ? 14 : 17);
     PREFETCH_RUN((cpu_mod == 3) ? 14 : 17, 3, rmdat, 1, 0, 0, 0, 0);
@@ -153,8 +173,12 @@ opIMUL_w_ib_a32(uint32_t fetchdat)
         tempw2 |= 0xff00;
 
     templ = ((int) tempw) * ((int) tempw2);
+    flags_rebuild();
+    if ((templ >> 15) != 0 && (templ >> 15) != -1)
+        cpu_state.flags |= C_FLAG | V_FLAG;
+    else
+        cpu_state.flags &= ~(C_FLAG | V_FLAG);
     cpu_state.regs[cpu_reg].w = templ & 0xffff;
-    setimul16(tempw, tempw2);
 
     CLOCK_CYCLES((cpu_mod == 3) ? 14 : 17);
     PREFETCH_RUN((cpu_mod == 3) ? 14 : 17, 3, rmdat, 1, 0, 0, 0, 1);
@@ -182,8 +206,12 @@ opIMUL_l_ib_a16(uint32_t fetchdat)
         templ2 |= 0xffffff00;
 
     temp64 = ((int64_t) templ) * ((int64_t) templ2);
+    flags_rebuild();
+    if ((temp64 >> 31) != 0 && (temp64 >> 31) != -1)
+        cpu_state.flags |= C_FLAG | V_FLAG;
+    else
+        cpu_state.flags &= ~(C_FLAG | V_FLAG);
     cpu_state.regs[cpu_reg].l = temp64 & 0xffffffff;
-    setimul32(templ, templ2);
 
     CLOCK_CYCLES(20);
     PREFETCH_RUN(20, 3, rmdat, 0, 1, 0, 0, 0);
@@ -210,8 +238,12 @@ opIMUL_l_ib_a32(uint32_t fetchdat)
         templ2 |= 0xffffff00;
 
     temp64 = ((int64_t) templ) * ((int64_t) templ2);
+    flags_rebuild();
+    if ((temp64 >> 31) != 0 && (temp64 >> 31) != -1)
+        cpu_state.flags |= C_FLAG | V_FLAG;
+    else
+        cpu_state.flags &= ~(C_FLAG | V_FLAG);
     cpu_state.regs[cpu_reg].l = temp64 & 0xffffffff;
-    setimul32(templ, templ2);
 
     CLOCK_CYCLES(20);
     PREFETCH_RUN(20, 3, rmdat, 0, 1, 0, 0, 1);
@@ -227,13 +259,15 @@ opIMUL_w_w_a16(uint32_t fetchdat)
     if (cpu_mod != 3)
         SEG_CHECK_READ(cpu_state.ea_seg);
 
-    int16_t op1 = cpu_state.regs[cpu_reg].w;
-    int16_t op2 = geteaw();
+    templ = (int32_t) (int16_t) cpu_state.regs[cpu_reg].w * (int32_t) (int16_t) geteaw();
     if (cpu_state.abrt)
         return 1;
-    templ = (int32_t) op1 * (int32_t) op2;
     cpu_state.regs[cpu_reg].w = templ & 0xFFFF;
-    setimul16(op1, op2);
+    flags_rebuild();
+    if ((templ >> 15) != 0 && (templ >> 15) != -1)
+        cpu_state.flags |= C_FLAG | V_FLAG;
+    else
+        cpu_state.flags &= ~(C_FLAG | V_FLAG);
 
     CLOCK_CYCLES(18);
     PREFETCH_RUN(18, 2, rmdat, 1, 0, 0, 0, 0);
@@ -248,13 +282,15 @@ opIMUL_w_w_a32(uint32_t fetchdat)
     if (cpu_mod != 3)
         SEG_CHECK_READ(cpu_state.ea_seg);
 
-    int16_t op1 = cpu_state.regs[cpu_reg].w;
-    int16_t op2 = geteaw();
+    templ = (int32_t) (int16_t) cpu_state.regs[cpu_reg].w * (int32_t) (int16_t) geteaw();
     if (cpu_state.abrt)
         return 1;
-    templ = (int32_t) op1 * (int32_t) op2;
     cpu_state.regs[cpu_reg].w = templ & 0xFFFF;
-    setimul16(op1, op2);
+    flags_rebuild();
+    if ((templ >> 15) != 0 && (templ >> 15) != -1)
+        cpu_state.flags |= C_FLAG | V_FLAG;
+    else
+        cpu_state.flags &= ~(C_FLAG | V_FLAG);
 
     CLOCK_CYCLES(18);
     PREFETCH_RUN(18, 2, rmdat, 1, 0, 0, 0, 1);
@@ -270,13 +306,15 @@ opIMUL_l_l_a16(uint32_t fetchdat)
     if (cpu_mod != 3)
         SEG_CHECK_READ(cpu_state.ea_seg);
 
-    int32_t op1 = cpu_state.regs[cpu_reg].l;
-    int32_t op2 = geteal();
+    temp64 = (int64_t) (int32_t) cpu_state.regs[cpu_reg].l * (int64_t) (int32_t) geteal();
     if (cpu_state.abrt)
         return 1;
-    temp64 = (int64_t) op1 * (int64_t) op2;
     cpu_state.regs[cpu_reg].l = temp64 & 0xFFFFFFFF;
-    setimul32(op1, op2);
+    flags_rebuild();
+    if ((temp64 >> 31) != 0 && (temp64 >> 31) != -1)
+        cpu_state.flags |= C_FLAG | V_FLAG;
+    else
+        cpu_state.flags &= ~(C_FLAG | V_FLAG);
 
     CLOCK_CYCLES(30);
     PREFETCH_RUN(30, 2, rmdat, 0, 1, 0, 0, 0);
@@ -291,13 +329,15 @@ opIMUL_l_l_a32(uint32_t fetchdat)
     if (cpu_mod != 3)
         SEG_CHECK_READ(cpu_state.ea_seg);
 
-    int32_t op1 = cpu_state.regs[cpu_reg].l;
-    int32_t op2 = geteal();
+    temp64 = (int64_t) (int32_t) cpu_state.regs[cpu_reg].l * (int64_t) (int32_t) geteal();
     if (cpu_state.abrt)
         return 1;
-    temp64 = (int64_t) op1 * (int64_t) op2;
     cpu_state.regs[cpu_reg].l = temp64 & 0xFFFFFFFF;
-    setimul32(op1, op2);
+    flags_rebuild();
+    if ((temp64 >> 31) != 0 && (temp64 >> 31) != -1)
+        cpu_state.flags |= C_FLAG | V_FLAG;
+    else
+        cpu_state.flags &= ~(C_FLAG | V_FLAG);
 
     CLOCK_CYCLES(30);
     PREFETCH_RUN(30, 2, rmdat, 0, 1, 0, 0, 1);
