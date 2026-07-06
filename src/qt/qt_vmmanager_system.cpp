7820e96714dc97f4956c2307dc4007db153ee6d8
OBattler <oubattler@gmail.com>
2026-07-06 23:14:34 +0200

    Manager: Fix the ignoring of 86box.cfg in the root of the VMM path in cases where the VMM path does not end with a slah or a backslash.

--- src/qt/qt_vmmanager_system.cpp
+++ src/qt/qt_vmmanager_system.cpp
@@ -164,7 +164,13 @@ VMManagerSystem::scanForConfigs(QWidget *parent, const QString &searchPath)
         progDialog.setLabelText(tr("Found %1").arg(QString::number(found)));
         QApplication::processEvents();
         QString filename = dir_iterator.next();
-        QString root_cfg = QString("%1%2").arg(search_directory, config_file_name);
+        char *sd = search_directory.toUtf8().data();
+        QString root_cfg;
+        if ((sd[strlen(sd) - 1] == '/') || (sd[strlen(sd) - 1] == '\\'))
+            root_cfg = QString("%1%2").arg(search_directory, config_file_name);
+        else
+            root_cfg = QString("%1/%2").arg(search_directory, config_file_name);
+        printf("\"%s\", \"%s\"\n", filename.toUtf8().data(), root_cfg.toUtf8().data());
         if (filename.compare(root_cfg, Qt::CaseInsensitive) != 0)
             matches.append(filename);
     }
