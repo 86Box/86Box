6088ffe45c59b6f6972ba44b2c5b9e5bf081c8fe
Cacodemon345 <wahil1976@outlook.com>
2026-07-08 22:58:25 +0600

    Cleaned up version of Deksor's real-time timer patches

--- src/qt/qt_main.cpp
+++ src/qt/qt_main.cpp
@@ -491,7 +491,7 @@ main_thread_fn()
                 frames     = 0;
             }
 
-            if (!fast_forward && debt_ns >= quantum_ns)
+            if (!cpu_uncapped && !fast_forward && debt_ns >= quantum_ns)
                 debt_ns -= quantum_ns;
             else
                 debt_ns = 0;
