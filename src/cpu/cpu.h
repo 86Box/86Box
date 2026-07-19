ec1949c3706c137184c18245454f7c76361bf96d
OBattler <oubattler@gmail.com>
2026-07-19 05:09:04 +0200

    cpu/cpu.h: Make the new function pointers extern.

--- src/cpu/cpu.h
+++ src/cpu/cpu.h
@@ -850,13 +850,13 @@ extern int cpu_override_interpreter;
 
 extern int is_lock_legal(uint32_t fetchdat);
 
-void     (*prefetch_queue_set_pos)(int pos);
-void     (*prefetch_queue_set_ip)(uint16_t ip);
-void     (*prefetch_queue_set_prefetching)(int p);
-int      (*prefetch_queue_get_pos)(void);
-uint16_t (*prefetch_queue_get_ip)(void);
-int      (*prefetch_queue_get_prefetching)(void);
-int      (*prefetch_queue_get_size)(void);
+extern void     (*prefetch_queue_set_pos)(int pos);
+extern void     (*prefetch_queue_set_ip)(uint16_t ip);
+extern void     (*prefetch_queue_set_prefetching)(int p);
+extern int      (*prefetch_queue_get_pos)(void);
+extern uint16_t (*prefetch_queue_get_ip)(void);
+extern int      (*prefetch_queue_get_prefetching)(void);
+extern int      (*prefetch_queue_get_size)(void);
 
 extern void    i808x_hook_prefetch_queue(void     (*pf_set_pos)(int pos),
                                          void     (*pf_set_ip)(uint16_t ip),
