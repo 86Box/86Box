7086d5b4e93fb88e84372c31a19151e06f1c551d
Maxwell Scott <tongpauz@gmail.com>
2026-07-26 15:48:38 +0700

    Added the ASUS ISA-386SIQ
    It is the 386DX machine with SiS 460 chipset, also seen with SiS 461 chipset.

--- src/machine/m_at_386dx.c
+++ src/machine/m_at_386dx.c
@@ -479,3 +479,26 @@ machine_at_asus386_init(const machine_t *model)
 
     return ret;
 }
+
+int
+machine_at_asus386siq_init(const machine_t *model)
+{
+    int ret;
+
+    ret = bios_load_linear("roms/machines/asus386siq/bios-657e297a703d5076313977.bin",
+                           0x000f0000, 65536, 0);
+
+    if (bios_only || !ret)
+        return ret;
+
+    machine_at_common_init(model);
+
+    device_add(&sis_85c460_device);
+
+    device_add_params(machine_get_kbc_device(machine), (void *) model->kbc_params);
+
+    if (fdc_current[0] == FDC_INTERNAL)
+        device_add(&fdc_at_device);
+
+    return ret;
+}
