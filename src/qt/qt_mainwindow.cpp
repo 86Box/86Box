ea73c48a90df6a7d486417ada10c9a511000d06b
Alexander Babikov <lemondrops358@gmail.com>
2026-07-29 15:18:03 +0500

    Renderer name changes:
    - Remove (3.0 Core) from the OpenGL renderer's name; with the old
    Qt (OpenGL) renderer gone, so is the need for disambiguation;
    - Add "&Vulkan" string to translation files, for shortcut
    consistency in CJK languages;
    - Change the shortcut for the VNC renderer.

--- src/qt/qt_mainwindow.cpp
+++ src/qt/qt_mainwindow.cpp
@@ -533,7 +533,7 @@ MainWindow::MainWindow(QWidget *parent)
             fprintf(stderr, "OpenGL renderers are unsupported on %s.\n", QApplication::platformName().toUtf8().data());
         vid_api = RENDERER_SOFTWARE;
         ui->actionVulkan->setVisible(false);
-        ui->actionOpenGL_3_0_Core->setVisible(false);
+        ui->actionOpenGL->setVisible(false);
     }
 
 #ifndef USE_VNC
@@ -565,7 +565,7 @@ MainWindow::MainWindow(QWidget *parent)
 
     auto actGroup = new QActionGroup(this);
     actGroup->addAction(ui->actionSoftware_Renderer);
-    actGroup->addAction(ui->actionOpenGL_3_0_Core);
+    actGroup->addAction(ui->actionOpenGL);
     actGroup->addAction(ui->actionVulkan);
     actGroup->addAction(ui->actionVNC);
     actGroup->setExclusive(true);
