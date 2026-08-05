a43fb2d7e2c2b8a74208103abf16e0066fbe4d7c
Hisuzu <185547947+sentogahara@users.noreply.github.com>
2026-08-05 23:52:10 +0300

    Add the MSI MS-5169

--- src/include/86box/machine.h
+++ src/include/86box/machine.h
@@ -1245,6 +1245,10 @@ extern int             machine_at_5aa_init(const machine_t *);
 extern const device_t  g5x_device;
 #endif
 extern int             machine_at_g5x_init(const machine_t *);
+#ifdef EMU_DEVICE_H
+extern const device_t  ms5169_device;
+#endif
+extern int             machine_at_ms5169_init(const machine_t *);
 
 /* VIA MVP3 */
 extern int             machine_at_ax59pro_init(const machine_t *);
