e5c6daafa53ddbcdd9eb66fbf94357181eb0ee60
David Hrdlička <13226155+dhrdlicka@users.noreply.github.com>
2026-08-09 21:59:02 +0200

    cmake: add warning when using old toolchain files

---
+++ cmake/flags-gcc.cmake
@@ -0,0 +1,6 @@
+message(WARNING [=[
+The 86Box toolchain files are deprecated and will be removed in a future
+release, as it is no longer necessary to use them to build the project with
+recommended flags. Please remove the -DCMAKE_TOOLCHAIN_FILE or --toolchain
+flag from your CMake command line.
+]=])
