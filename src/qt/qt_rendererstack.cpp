ce3cb928c163690dc88ef67ed2305fac6c433df7
Cacodemon345 <wahil1976@outlook.com>
2026-08-14 23:38:18 +0600

    Ensure that the non-primary Vulkan renderers are shown

--- src/qt/qt_rendererstack.cpp
+++ src/qt/qt_rendererstack.cpp
@@ -450,6 +450,11 @@ RendererStack::createRenderer(Renderer renderer)
                     emit rendererChanged();
                     if (m_monitor_index != 0 && show_second_monitors)
                         this->show();
+
+                    QTimer::singleShot(1000, [this] {
+                        if (m_monitor_index != 0 && show_second_monitors)
+                            this->show();
+                    });
                 });
                 connect(hw, &VulkanWindowRenderer::errorInitializing, [=]() {
                     /* Renderer could not initialize, fallback to software. */
