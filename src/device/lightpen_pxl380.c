83157265adde1a060f688af1216e44ab85f3531c
Cacodemon345 <wahil1976@outlook.com>
2026-08-17 00:26:01 +0600

    Finalize PXL-380 implementation

--- src/device/lightpen_pxl380.c
+++ src/device/lightpen_pxl380.c
@@ -1,3 +1,19 @@
+/*
+ * 86Box    A hypervisor and IBM PC system emulator that specializes in
+ *          running old operating systems and software designed for IBM
+ *          PC systems and compatibles from 1981 through fairly recent
+ *          system designs based on the PCI bus.
+ *
+ *          This file is part of the 86Box distribution.
+ *
+ *          Implementation of PXL-380 lightpen.
+ *
+ * Authors: Cacodemon345
+ *          Daniel Balsom
+ *
+ *          Copyright 2026 Cacodemon345.
+ *          Copyright 2022-2026 Daniel Balsom
+ */
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdint.h>
@@ -30,6 +46,8 @@ struct pxl380_t
 
     uint8_t status;
     pc_timer_t timer;
+
+    float luma_thresh;
 };
 
 typedef struct pxl380_t pxl380_t;
@@ -61,7 +79,7 @@ pxl380_sample_luma(bitmap_t* target_buffer, uint32_t x, uint32_t y)
 
 void pxl380_lightpen_check_trigger_strobe(void* priv, int x_offset, int y, int x_offset_from_hsync, int firstline, double pix_clock, int monitor_used)
 {
-    pxl380_t* pxl380 = priv;
+    pxl380_t* pxl380 = (pxl380_t*)priv;
     double abs_x, abs_y;
 
     mouse_get_abs_coords(&abs_x, &abs_y);
@@ -69,20 +87,18 @@ void pxl380_lightpen_check_trigger_strobe(void* priv, int x_offset, int y, int x
     abs_x *= monitors[monitor_used].mon_unscaled_size_x - 1;
     abs_y *= monitors[monitor_used].mon_unscaled_size_y - 1;
 
-    int x = abs_x + x_offset;
+    int x = abs_x + (!enable_overscan ? x_offset : 0);
     float sampled_luma = pxl380_sample_luma(monitors[monitor_used].target_buffer, x, y);
     int real_x = x; // This is the position from the actual beginning of the render.
     x -= x_offset;
-    y -= firstline;
+    y -= firstline + monitors[monitor_used].mon_overscan_y / 2;
     
     if (enable_overscan) {
-        x += x_offset;
         y += monitors[monitor_used].mon_overscan_y / 2;
-        x_offset = 0;
     }
 
     // Checking for x makes no sense.
-    if (sampled_luma >= 0.125 && y == (int)abs_y) {
+    if (sampled_luma >= pxl380->luma_thresh && y == (int)abs_y) {
         double factor = 48000000. / pix_clock;
         int latch_val = (x_offset_from_hsync + real_x) * factor;
 
@@ -97,7 +113,7 @@ void pxl380_lightpen_check_trigger_strobe(void* priv, int x_offset, int y, int x
 
 void pxl380_lightpen_hsync(void* priv)
 {
-    pxl380_t* pxl380 = priv;
+    pxl380_t* pxl380 = (pxl380_t*)priv;
 
     pxl380->timer0_cnt++;
     pxl380->timer1_cnt_dummy = 0;
@@ -105,7 +121,7 @@ void pxl380_lightpen_hsync(void* priv)
 
 void pxl380_lightpen_vsync(void* priv)
 {
-    pxl380_t* pxl380 = priv;
+    pxl380_t* pxl380 = (pxl380_t*)priv;
 
     pxl380->timer0_cnt = 0;
 
@@ -119,11 +135,11 @@ void pxl380_lightpen_vsync(void* priv)
 uint8_t
 pxl380_read(uint16_t addr, void* priv)
 {
-    pxl380_t* pxl380 = priv;
+    pxl380_t* pxl380 = (pxl380_t*)priv;
     uint8_t   ret = 0x00;
     switch (addr & 7) {
         case 0:
-            ret = ~(pxl380->status | ((tablet_get_buttons_ex() & 3) << 1));
+            ret = ~(pxl380->status | ((tablet_get_buttons_ex() & 1) << 2)) | ((tablet_get_buttons_ex() & 2) << 1);
             break;
         case 1:
             ret = ((pxl380->timer1_cnt_latch >> 8) & 0xf) | (((pxl380->timer0_cnt_latch >> 8) & 0xf) << 4);
@@ -159,61 +175,20 @@ pxl380_read(uint16_t addr, void* priv)
         {
             pxl380->irq_enabled = 1;
             pxl380->status &= ~1;
+            picintc(1 << pxl380->irq_num);
             break;
         }
         default:
             break;
     }
 
-    //pclog("[PXL-380] RET 0x%02X (PORT %d)\n", ret, addr & 7);
     return ret;
-}
-
-void
-pxl380_write(uint16_t addr, uint8_t val, void* priv)
-{
-    pxl380_t* pxl380 = priv;
-    //pclog("[PXL-380] WRITE 0x%02X (VAL 0x%02X)\n", addr, val);
-    switch (addr & 7) {
-        case 4:
-        {
-            pxl380->status &= ~0x8;
-            break;
-        }
-        case 5:
-        {
-            if (!(pxl380->status & 0x8)) {
-                pxl380->timer1_cnt_latch = pxl380->timer1_cnt_dummy & 0xfff;
-                pxl380->timer0_cnt_latch = pxl380->timer0_cnt & 0xfff;
-                pxl380->status |= 0x8;
-                if (pxl380->irq_enabled)
-                    picint(1 << pxl380->irq_num);
-            }
-            break;
-        }
-        case 6:
-        {
-            pxl380->irq_enabled = 0;
-            break;
-        }
-        case 7:
-        {
-            pxl380->irq_enabled = 1;
-            pxl380->status &= ~1;
-            break;
-        }
-
-        default:
-            return;
-    }
-
-    return;
 }
 
 void
 pxl380_timer1_cntr(void *priv)
 {
-    pxl380_t* pxl380 = priv;
+    pxl380_t* pxl380 = (pxl380_t*)priv;
 
     pxl380->timer1_cnt_dummy++;
     timer_on_auto(&pxl380->timer, (1. / 48000000.) * 1000. * 1000.);
@@ -221,14 +196,16 @@ pxl380_timer1_cntr(void *priv)
 
 void* pxl380_init(const device_t* info)
 {
-    pxl380_t* pxl380 = calloc(1, sizeof(pxl380_t));
+    pxl380_t* pxl380 = (pxl380_t*)calloc(1, sizeof(pxl380_t));
 
     timer_add(&pxl380->timer, pxl380_timer1_cntr, pxl380, 0);
     timer_on_auto(&pxl380->timer, (1. / 48000000.) * 1000. * 1000.);
-    io_sethandler(device_get_config_hex16("base"), 8, pxl380_read, NULL, NULL, pxl380_write, NULL, NULL, pxl380);
+    io_sethandler(device_get_config_hex16("base"), 8, pxl380_read, NULL, NULL, NULL, NULL, NULL, pxl380);
     pxl380->irq_num = device_get_config_int("irq");
 
     video_lightpen_set_callbacks(pxl380, pxl380_lightpen_hsync, pxl380_lightpen_vsync, pxl380_lightpen_check_trigger_strobe);
+
+    pxl380->luma_thresh = device_get_config_int("luma_thresh") / 100.;
 
     mouse_set_buttons(2);
     mouse_set_poll_ex(NULL, NULL);
@@ -278,6 +255,17 @@ static const device_config_t pxl380_config[] = {
             { .description = "0x370", .value = 0x370 },
             { .description = ""                      }
         },
+        .bios           = { { 0 } }
+    },
+    {
+        .name           = "luma_thresh",
+        .description    = "Luminance threshold (%)",
+        .type           = CONFIG_SPINNER,
+        .default_string = NULL,
+        .default_int    = 12,
+        .file_filter    = NULL,
+        .spinner        = { .min = 0, .max = 100, .step = 1 },
+        .selection      = { { 0 } },
         .bios           = { { 0 } }
     },
     { .name = "", .description = "", .type = CONFIG_END }
