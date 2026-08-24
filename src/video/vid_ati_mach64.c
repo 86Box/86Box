e70611b874f0f6e411de286773e0529f6f219991
OBattler <oubattler@gmail.com>
2026-08-24 11:50:34 +0200

    ATi Mach64: And another banking fix.

--- src/video/vid_ati_mach64.c
+++ src/video/vid_ati_mach64.c
@@ -1352,13 +1352,13 @@ mach64_ext_outb(uint16_t port, uint8_t val, void *priv)
         switch (port_high)
         {
              case 0x56: // 56ec-56ef
-                if ((port_low == 0xED) || (port_low == 0xEF))
+                if (port_low == 0xEF)
                     break;
 
                 if (port_low == 0xEC)                 
                     mach64_ext_writeb(0x400 | 0xb4, val, priv);
                 else
-                    mach64_ext_writeb(0x400 | 0xb6, val, priv);
+                    mach64_ext_writeb(0x400 | 0xb5, val, priv);
                 break; 
             case 0x5a: // 5aec-5aef
                 if (port_low == 0xEF)
@@ -1367,7 +1367,7 @@ mach64_ext_outb(uint16_t port, uint8_t val, void *priv)
                 if (port_low == 0xEC)                 
                     mach64_ext_writeb(0x400 | 0xb8, val, priv);
                 else
-                    mach64_ext_writeb(0x400 | 0xba, val, priv);
+                    mach64_ext_writeb(0x400 | 0xb9, val, priv);
                 break; 
             case 0x5e: // 5eec-5eef
                 if (mach64->type == MACH64_GX)
