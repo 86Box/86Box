b765785f14f6e654f9199767ee7ae16cc858177f
Hisuzu <185547947+sentogahara@users.noreply.github.com>
2026-08-26 09:00:30 +0300

    Add BIOS version option for the latest stock AN430TX BIOS and set it as default

--- src/machine/m_at_socket7.c
+++ src/machine/m_at_socket7.c
@@ -1939,7 +1939,7 @@ static const device_config_t an430tx_config[] = {
             },
             {
                 .name          = "PhoenixBIOS 4.0 Release 6.0 - Revision P09-0006 (Packard Bell PB79x)",
-                .internal_name = "an430tx",
+                .internal_name = "pb79x",
                 .bios_type     = BIOS_NORMAL,
                 .files_no      = 5,
                 .local         = 0,
@@ -1947,6 +1947,17 @@ static const device_config_t an430tx_config[] = {
                 .files         = { "roms/machines/an430tx/ANP0911A.BIO", "roms/machines/an430tx/ANP0911A.BI1",
                                    "roms/machines/an430tx/ANP0911A.BI2", "roms/machines/an430tx/ANP0911A.BI3",
                                    "roms/machines/an430tx/ANP0911A.RCV", "" }
+            },
+            {
+                .name          = "PhoenixBIOS 4.0 Release 6.0 - Revision P10-0095",
+                .internal_name = "an430tx",
+                .bios_type     = BIOS_NORMAL,
+                .files_no      = 5,
+                .local         = 0,
+                .size          = 262144,
+                .files         = { "roms/machines/an430tx/P10-0095.BIO", "roms/machines/an430tx/P10-0095.BI1",
+                                   "roms/machines/an430tx/P10-0095.BI2", "roms/machines/an430tx/P10-0095.BI3",
+                                   "roms/machines/an430tx/P10-0095.RCV", "" }
             },
             { .files_no = 0 }
         }
