b78dd0c0d00bbdc155c4d830c5e6bad631eee781
Mihai NADAS <mihai@nadas.ro>
2026-08-26 14:29:35 +0300

    Input: Prevent menu activation on mouse release

--- src/unix/sdl_main.c
+++ src/unix/sdl_main.c
@@ -617,6 +617,9 @@ main(int argc, char **argv)
                             const int key_down = event.key.down;
 #endif
                             if (key_down && (keyboard_get_shift() & 0x11) && (keyboard_get_shift() & 0x44) && (xtkey == 0x22) && mouse_capture) {
+                                /* Prevent an Alt-based shortcut from looking like a standalone
+                                 * Alt press to the guest when the held modifiers are released. */
+                                keyboard_input(0, xtkey);
                                 keyboard_all_up();
                                 plat_mouse_capture(0);
                                 break;
