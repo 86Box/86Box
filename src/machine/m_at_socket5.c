87b75ffc9cf91f6f0345eb93e9313a178f9176dc
Hisuzu <185547947+sentogahara@users.noreply.github.com>
2026-09-02 20:41:14 +0300

    Add the Packard Bell PB570 (Hillary) machine

--- src/machine/m_at_socket5.c
+++ src/machine/m_at_socket5.c
@@ -882,6 +882,39 @@ machine_at_powermatev_init(const machine_t *model)
 }
 
 int
+machine_at_pb570_init(const machine_t *model)
+{
+    int ret;
+
+    ret = bios_load_linear_combined("roms/machines/pb570/1007BY0R.BIO",
+                                    "roms/machines/pb570/1007BY0R.BI1",
+                                    0x1d000, 128);
+
+    if (bios_only || !ret)
+        return ret;
+
+    machine_at_common_init(model);
+
+    pci_init(PCI_CONFIG_TYPE_1);
+    pci_register_slot(0x00, PCI_CARD_NORTHBRIDGE, 0, 0, 0, 0);
+    pci_register_slot(0x08, PCI_CARD_VIDEO,       4, 0, 0, 0);
+    pci_register_slot(0x0B, PCI_CARD_NORMAL,      3, 2, 1, 4);
+    pci_register_slot(0x11, PCI_CARD_NORMAL,      1, 3, 2, 4);
+    pci_register_slot(0x13, PCI_CARD_NORMAL,      2, 1, 3, 4);
+    pci_register_slot(0x07, PCI_CARD_SOUTHBRIDGE, 0, 0, 0, 0);
+
+    if (gfxcard[0] == VID_INTERNAL)
+        device_add(machine_get_vid_device(machine));
+
+    device_add(&i430fx_device);
+    device_add(&piix_device);
+    device_add_params(&pc87306_device, (void *) PCX730X_AMI);
+    device_add(&intel_flash_bxt_ami_device);
+
+    return ret;
+}
+
+int
 machine_at_sjp54csr_init(const machine_t *model)
 {
     int ret;
