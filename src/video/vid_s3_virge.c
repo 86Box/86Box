f08d9d2c645885abd46770fd772991243f3b8236
Alexander Babikov <lemondrops358@gmail.com>
2026-09-06 01:37:48 +0500

    MGA/ViRGE/Banshee: Enable the PCI capabilities bit in status register for AGP cards

--- src/video/vid_s3_virge.c
+++ src/video/vid_s3_virge.c
@@ -5616,6 +5616,10 @@ s3_virge_pci_read(UNUSED(int func), int addr, UNUSED(int len), void *priv)
             ret = virge->pci_regs[PCI_REG_COMMAND] & 0x27;
             break;
 
+        case 0x06:
+            ret = (virge->chip >= S3_VIRGEGX2) ? PCI_STATUS_L_CAPAB : 0x00;
+            break;
+
         case 0x07:
             ret = virge->pci_regs[0x07] & 0x36;
             break;
