d3a40835dc44e26bf931583a07bbdd16b9aa643f
mw308 <34479591+mw308@users.noreply.github.com>
2026-09-05 22:40:05 +0100

    Re-fix the Solo-1's name

--- src/sound/snd_solo1.c
+++ src/sound/snd_solo1.c
@@ -1104,7 +1104,7 @@ solo1_close(void *priv)
 }
 
 const device_t ess_solo1_device = {
-    .name          = "ESS Solo-1",
+    .name          = "ESS Solo-1 ES1938S",
     .internal_name = "ess_solo1",
     .flags         = DEVICE_PCI,
     .local         = 0,
@@ -1119,7 +1119,7 @@ const device_t ess_solo1_device = {
 };
 
 const device_t ess_solo1_onboard_device = {
-    .name          = "ESS Solo-1 (On-Board)",
+    .name          = "ESS Solo-1 ES1938S (On-Board)",
     .internal_name = "ess_solo1_onboard",
     .flags         = DEVICE_PCI,
     .local         = 1,
