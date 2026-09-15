9f868eb28a439c1c0cb05fbb540a7ef39de64bd1
OBattler <oubattler@gmail.com>
2026-09-15 23:13:54 +0200

    AT NVR: Force the VRT bit to set when loading an existing NVR, fixes loading old NVR's saved with the VRT bit clear (because it was returning forced set anyway), fixes #7965.

--- src/nvr_at.c
+++ src/nvr_at.c
@@ -983,6 +983,9 @@ nvr_start(nvr_t *nvr)
     if (default_found == nvr->size)
         nvr->regs[0x0e] = 0xff; /* If load failed or it loaded an uninitialized NVR,
                                    mark everything as bad. */
+    else if (!nvr->is_new)
+        nvr->regs[0x0d] = REGD_VRT; /* If it is not new then set the VRT bit to
+                                       valid. */
 
     /* Initialize the internal and chip times. */
     if (time_sync & TIME_SYNC_ENABLED) {
