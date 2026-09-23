e92261e37ab3de5e719fa3a5bbb1b2ccaedd7d6d
Maxwell Scott <tongpauz@gmail.com>
2026-09-23 15:29:44 +0700

    Misc. changes (23/09/26)
    1. Make AIR 54TDP as a Socket 7-SV machine has it does not support MMX CPUs according to the manual (it doesn't mention MMX CPUs) & minimize minimum & step RAM memory
    2. Alphabetize & shorten the names for MCA IBM network cards

--- src/device.c
+++ src/device.c
@@ -253,6 +253,9 @@ device_set_context(device_context_t *ctx, const device_t *dev, int inst)
         { .old = "Gravis UltraSound PnP (New PnP ROM)", .new = "Gravis UltraSound PnP (New)" },
         { .old = "Gravis UltraSound PnP (No CD-ROM)", .new = "Gravis UltraSound PnP (No CD)" },
         { .old = "Compaq/STB UltraSound 32", .new = "Compaq UltraSound 32" },
+        { .old = "IBM PS/2 Adapter/A for Ethernet Networks (WD8013WP/A, AUI/RJ-45, EFD4/92F0046)", .new = "IBM PS/2 Adapter/A (WD8013WP/A)" },
+        { .old = "IBM PS/2 Adapter/A for Ethernet Networks (WD8013EP/A, AUI/BNC, EFD5)", .new = "IBM PS/2 Adapter/A (WD8013EP/A)" },
+        { .old = "IBM PS/2 Adapter/A for Ethernet Networks (WD8003E/A, AUI/BNC, EFE5)", .new = "IBM PS/2 Adapter/A (WD8003E/A)" },
         { 0 }
     };
 
